02cfe25242f957eb9ed367964d7f9de87d2fb4cd
Pixeladed <vietminhle98@gmail.com>
2019-03-11 00:15:00 +0800

    feat(lint): Add eslint
    Add eslint. Add standard code style support. Add react typescript plugins

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
-const withTypescript = require("@zeit/next-typescript");
-const withSass = require("@zeit/next-sass");
-const withSourceMaps = require("@zeit/next-source-maps");
+const withTypescript = require('@zeit/next-typescript')
+const withSass = require('@zeit/next-sass')
+const withSourceMaps = require('@zeit/next-source-maps')
 
-module.exports = withSourceMaps(withTypescript(withSass()));
+module.exports = withSourceMaps(withTypescript(withSass()))
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,4 +2,4 @@ export default (props: {}) => (
   <div>
     <h1>Hello world</h1>
   </div>
-);
+)
