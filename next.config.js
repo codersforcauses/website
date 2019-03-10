3238545cb725b8ae031f9f6562c0f5cccc9314ed
Pixeladed <vietminhle98@gmail.com>
2019-03-11 00:02:23 +0800

    feat: Add sass and sourcemaps support
    Add @zeit/next-sass and @zeit/next-source-maps plugins

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
-// next.config.js
 const withTypescript = require("@zeit/next-typescript");
+const withSass = require("@zeit/next-sass");
+const withSourceMaps = require("@zeit/next-source-maps");
+
 module.exports = withTypescript();
+module.exports = withSass();
+module.exports = withSourceMaps();
