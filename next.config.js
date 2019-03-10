1e8b89194a228fa6155e77803d41e0537f814e3f
Pixeladed <vietminhle98@gmail.com>
2019-03-11 00:06:39 +0800

    fix(ssr): Fix next plugins crashing server rendering

--- next.config.js
+++ next.config.js
@@ -2,6 +2,4 @@ const withTypescript = require("@zeit/next-typescript");
 const withSass = require("@zeit/next-sass");
 const withSourceMaps = require("@zeit/next-source-maps");
 
-module.exports = withTypescript();
-module.exports = withSass();
-module.exports = withSourceMaps();
+module.exports = withSourceMaps(withTypescript(withSass()));
