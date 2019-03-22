d762a092a123f8fd7961e958f4ff3cb3def413c5
JeremiahPinto <jeremiahpinto279@outlook.com>
2019-03-22 14:29:06 +0800

    feat(icons): Added social icons and removed links

--- components/Team/CardItem/index.tsx
+++ components/Team/CardItem/index.tsx
@@ -1,43 +1,87 @@
 import React from 'react'
-import { Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText, CardProps, Button } from 'reactstrap'
+import { Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText, CardProps, ListGroup, ListGroupItem } from 'reactstrap'
 
 export default function (props: Props) {
   const { item } = props
   const { name, position, about, social, picture } = item
   return (
-    <Card className='m-3'>
+    <Card className='m-3 border-0'>
+      {style}
       <CardImg top width='100%' src={picture.src} alt={picture.alt} />
-      <CardBody>
+      <CardBody className='px-0'>
         <CardTitle className='font-weight-bolder text-monospace'>
-          {position}
+          {name}
         </CardTitle>
-        <CardSubtitle>{name}</CardSubtitle>
-        <CardText>{about}</CardText>
-        <Button color='link'>link</Button>
+        <CardSubtitle className='mb-1'>
+          {position}
+        </CardSubtitle>
+        <CardText className='mb-1'>
+          {about}
+        </CardText>
+        <CardText className='mb-1'>
+          <a href={'mailto:' + social.email} className='text-muted' title={social.email}>
+            <i className='fas fa-envelope icon' />
+          </a>
+          {social.github !== undefined &&
+            <a target='_blank' href={social.github} className='text-muted'>
+              <i className='fab fa-github-square icon' />
+            </a>
+          }
+          {social.gitlab !== undefined &&
+            <a target='_blank' href={social.gitlab} className='text-muted'>
+              <i className='fab fa-gitlab icon' />
+            </a>
+          }
+          {social.bitbucket !== undefined &&
+            <a target='_blank' href={social.bitbucket} className='text-muted'>
+              <i className='fab fa-bitbucket icon' />
+            </a>
+          }
+          {social.linkedin !== undefined &&
+            <a target='_blank' href={social.linkedin} className='text-muted'>
+              <i className='fab fa-linkedin icon' />
+            </a>
+          }
+          {social.facebook !== undefined &&
+            <a target='_blank' href={social.facebook} className='text-muted'>
+              <i className='fab fa-facebook icon' />
+            </a>
+          }
+          {social.twitter !== undefined &&
+            <a target='_blank' href={social.twitter} className='text-muted'>
+              <i className='fab fa-twitter-square icon' />
+            </a>
+          }
+        </CardText>
       </CardBody>
     </Card>
   )
 }
-
+const style = (
+  <style jsx>{`
+    .icon {
+      font-size: 1.4rem;
+      margin-right: 0.5rem;
+      vertical-align: middle;
+    }
+    .icon:hover {
+      color: black;
+    }
+  `}</style>
+)
 interface Props extends CardProps {
   item: CardItemContent
 }
 
-interface Link {
-  name: string
-  href: string
-}
-
 interface Social {
-  email?: string
+  email: string
   discord: string
-  github: Link
-  gitlabs?: Link
-  bitbucket?: Link
-  facebook?: Link
-  linkedin?: Link
-  twitter?: Link
-  website?: Link
+  github?: string
+  gitlab?: string
+  bitbucket?: string
+  linkedin?: string
+  facebook?: string
+  twitter?: string
 }
 
 interface Picture {
--- components/Team/index.tsx
+++ components/Team/index.tsx
@@ -11,14 +11,8 @@ export default class CommitteeCard extends Component {
       social: {
         email: 'president@codersforcauses.org',
         discord: 'Smooth Jezz',
-        github: {
-          name: 'JeremiahPinto',
-          href: 'https://github.com/JeremiahPinto'
-        },
-        linkedin: {
-          name: 'Jeremiah Pinto',
-          href: 'https://www.linkedin.com/in/jeremiah-pinto-3b5201160/'
-        }
+        github: 'https://github.com/JeremiahPinto',
+        linkedin: 'https://www.linkedin.com/in/jeremiah-pinto-3b5201160/'
       },
       picture: {
         src: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180',
@@ -32,10 +26,8 @@ export default class CommitteeCard extends Component {
       social: {
         email: 'vicepresident@codersforcauses.org',
         discord: 'Drubi',
-        github: {
-          name: 'DrewAlexander98',
-          href: 'https://github.com/DrewAlexander98'
-        }
+        github: 'https://github.com/DrewAlexander98',
+        linkedin: 'https://www.linkedin.com/in/drew-alexander-29397317a/'
       },
       picture: {
         src: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180',
@@ -49,10 +41,8 @@ export default class CommitteeCard extends Component {
       social: {
         email: 'secretary@codersforcauses.org',
         discord: 'Elysian',
-        github: {
-          name: 'ekinbukulmez',
-          href: 'https://github.com/ekinbukulmez'
-        }
+        github: 'https://github.com/ekinbukulmez',
+        linkedin: 'https://www.linkedin.com/in/ekinbukulmez/'
       },
       picture: {
         src: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180',
@@ -66,10 +56,8 @@ export default class CommitteeCard extends Component {
       social: {
         email: 'treasurer@codersforcauses.org',
         discord: 'davidyu112311234',
-        github: {
-          name: 'noobling',
-          href: 'https://github.com/noobling'
-        }
+        github: 'https://github.com/noobling',
+        linkedin: 'https://www.linkedin.com/in/david-yu-australia/'
       },
       picture: {
         src: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180',
@@ -83,10 +71,7 @@ export default class CommitteeCard extends Component {
       social: {
         email: 'clientofficer@codersforcauses.org',
         discord: 'Eagle-i',
-        github: {
-          name: 'innocentmm',
-          href: 'https://github.com/innocentmm'
-        }
+        github: 'https://github.com/innocentmm'
       },
       picture: {
         src: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180',
@@ -100,10 +85,8 @@ export default class CommitteeCard extends Component {
       social: {
         email: 'publicity@codersforcauses.org',
         discord: 'Minhocent Bystander',
-        github: {
-          name: 'Pixeladed',
-          href: 'https://github.com/Pixeladed'
-        }
+        github: 'https://github.com/Pixeladed',
+        linkedin: 'https://www.linkedin.com/in/vietminhle/'
       },
       picture: {
         src: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180',
@@ -117,10 +100,7 @@ export default class CommitteeCard extends Component {
       social: {
         email: 'committee@codersforcauses.org',
         discord: 'Tai',
-        github: {
-          name: 'thaingu',
-          href: 'https://github.com/thaingu'
-        }
+        github: 'https://github.com/thaingu'
       },
       picture: {
         src: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180',
@@ -134,10 +114,7 @@ export default class CommitteeCard extends Component {
       social: {
         email: 'committee@codersforcauses.org',
         discord: 'Happy Dust',
-        github: {
-          name: 'JKKim98',
-          href: 'https://github.com/JKKim98'
-        }
+        github: 'https://github.com/JKKim98'
       },
       picture: {
         src: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180',
@@ -151,10 +128,8 @@ export default class CommitteeCard extends Component {
       social: {
         email: 'committee@codersforcauses.org',
         discord: 'Joshua Milambo',
-        github: {
-          name: 'Papajmil',
-          href: 'https://github.com/Papajmil'
-        }
+        github: 'https://github.com/Papajmil',
+        linkedin: 'https://www.linkedin.com/in/joshua-milambo-8298aa163/'
       },
       picture: {
         src: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180',
