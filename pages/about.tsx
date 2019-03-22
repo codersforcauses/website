7b7dcd7138ea7a282789a76208b1d6818562c5c0
JeremiahPinto <jeremiahpinto279@outlook.com>
2019-03-22 14:30:23 +0800

    feat(logo): Added full-logo to about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,34 +1,36 @@
 import React from 'react'
-import { Container, Row, Col } from 'reactstrap'
+import { Jumbotron, Container, Row, Col } from 'reactstrap'
 import CommitteeCard from '../components/Team'
 
 export default (props: {}) => (
   <div>
     {style}
+    <Jumbotron className='mb-0 bg-dark hero pad text-white'>
+      <Container>
+        <Row className='my-5 d-flex align-items-center'>
+          <Col xs='12' lg='7'>
+            <h3 className='header'>
+              We build software for charities
+            </h3>
+            <p>
+              Coders for Causes is a not for profit organisation that empowers
+              charities and other not for profit organisations by connecting them
+              with university students to develop technical solutions. We are a
+              student-run club based in Perth, Western Australia with a wide range
+              of clients. Whether you are looking for technical advice or a long
+              term project, get in touch with us for more information.
+            </p>
+          </Col>
+
+          <Col xs='12' lg='5'>
+            <img src='/static/wordmark_white.svg' className='header-img' alt='Coders for Causes logo' />
+          </Col>
+        </Row>
+      </Container>
+    </Jumbotron>
+
     <Container>
-      <Row className='hero pad-4 bg-white d-flex align-items-center'>
-        <Col xs='12' lg='7'>
-          <h3 className='header'>
-            We build software for charities
-          </h3>
-          <p>
-            Coders for Causes is a non for profit organisation that empowers
-            charities and other non for profit organisations by connecting them
-            with university students to develop technical solutions. We are a
-            student-run club based in Perth, Western Australia with a wide range
-            of clients. Whether you are looking for technical advice or a long
-            term project, get in touch with us for more information.
-          </p>
-        </Col>
-
-        <Col xs='12' lg='5' className='border border-dark w-100 h-100' />
-      </Row>
-    </Container>
-
-    <hr />
-
-    <Container>
-      <Row className='pad-4'>
+      <Row className='pad'>
         <Col xs='12'>
           <h3 className='header'>
             Meet the Committee
@@ -38,15 +40,20 @@ export default (props: {}) => (
           <CommitteeCard />
         </Col>
       </Row>
-
-      <Row className='pad-4'>
-        <Col xs='12'>
-          <h3 className='header'>
-            Our Clients
-          </h3>
-        </Col>
-      </Row>
     </Container>
+
+    <Jumbotron className='pad m-0'>
+      <Container>
+        <Row>
+          <Col xs='12'>
+            <h3 className='header'>
+              Our Clients
+            </h3>
+          </Col>
+          <Col />
+        </Row>
+      </Container>
+    </Jumbotron>
   </div>
 )
 
@@ -56,24 +63,16 @@ const style = (
       font-weight: bolder;
       margin-bottom: 1.2rem;
     }
-    .pad-4 {
-      padding: 4rem 0;
+    .pad {
+      padding: 6.6rem 0;
     }
     .hero {
-      height: 40vh;
-      min-height: 26rem;
-      max-height: 50rem;
       margin-top: 4.5rem;
+      border-radius: 0;
     }
-    hr {
-      width: 100vw;
-      left: 0;
-      right:0;
-      background-color: black;
-      margin: 0;
-    }
-    .team {
-      
+    .header-img {
+      max-width: 500px;
+      margin-top: 1rem;
     }
   `}</style>
 )
