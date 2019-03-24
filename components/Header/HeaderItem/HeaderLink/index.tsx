a27cd845db558e010da5a0c6eb26c80c8d0148e5
Pixeladed <vietminhle98@gmail.com>
2019-03-24 16:39:09 +0800

    feat(header): make header responsive

--- components/Header/HeaderItem/HeaderLink/index.tsx
+++ components/Header/HeaderItem/HeaderLink/index.tsx
@@ -16,6 +16,7 @@ export default function (props: Props) {
 
 const style = (
   <style jsx>{`
+    .navbar-dark .navbar-nav .nav-link,
     a.header-link {
       color: #ffffff;
       cursor: pointer;
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,44 +1,76 @@
 import React, { Component } from 'react'
-import { Navbar, NavbarBrand, Container, Nav, Button } from 'reactstrap'
+import {
+  Navbar,
+  NavbarBrand,
+  Container,
+  Nav,
+  Button,
+  Collapse,
+  NavbarToggler
+} from 'reactstrap'
 import Link from 'next/link'
 import HeaderItem, { HeaderItemContent } from './HeaderItem'
 
-export default class Header extends Component {
+export default class Header extends Component<{}, State> {
+  state = {
+    isOpen: false
+  }
+
   links: HeaderItemContent[] = [
     {
       href: '/about',
       text: 'About',
       items: [
-        { text: 'Team', href: '/about' },
-        { text: 'Sponsors', href: '/about' },
-        { text: 'Contact', href: '/about' }
+        { text: 'Team', href: '/about/team' },
+        { text: 'Sponsors', href: '/about/sponsors' },
+        { text: 'Contact', href: '/about/contact' }
       ]
     },
     { href: '/events', text: 'Events' },
     { href: '/projects', text: 'Projects' }
   ]
 
+  toggleOpen = () => this.setState({ isOpen: !this.state.isOpen })
+
   render () {
+    const { isOpen } = this.state
     return (
-      <Navbar color='dark' className='text-white py-3 fixed-top shadow-sm' dark>
+      <Navbar
+        color='dark'
+        className='text-white py-3 fixed-top shadow-sm'
+        dark
+        expand='md'
+      >
         <Container>
           <Nav className='justify-content-start'>
+            <NavbarToggler
+              className='mr-4 d-block d-md-none d-flex align-items-center pl-0 border-0'
+              onClick={this.toggleOpen}
+            >
+              <i className='fas fa-bars text-white' />
+            </NavbarToggler>
             <Link href='/'>
-              <NavbarBrand className='mr-5 pr-5'>
+              <NavbarBrand className='mr-md-5 pr-md-5'>
                 <code className='font-weight-bold'>cfc</code>
               </NavbarBrand>
             </Link>
-            <Nav>
-              {this.links.map(link => (
-                <HeaderItem item={link} key={link.text} />
-              ))}
-            </Nav>
+            <Collapse navbar isOpen={isOpen}>
+              <Nav navbar>
+                {this.links.map(link => (
+                  <HeaderItem item={link} key={link.text} />
+                ))}
+              </Nav>
+            </Collapse>
           </Nav>
-          <Button color='secondary' outline>
+          <Button color='secondary' outline className='d-none d-md-block'>
             Membership
           </Button>
         </Container>
       </Navbar>
     )
   }
+}
+
+interface State {
+  isOpen: boolean
 }
