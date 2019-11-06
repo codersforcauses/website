1b3282508c34d15efd5fee659dee1330712c520f
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2019-11-06 09:55:31 +0800

    improvement(about): added committee cards and modified about to add previous changes

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,36 +1,37 @@
 import React from 'react'
 import { Jumbotron, Container, Row, Col } from 'reactstrap'
-import CommitteeCard from '../components/Team'
+import { CommitteeCard } from '../components/Committee'
+import Clients from '../components/Clients'
+import Sponsors from '../components/Sponsors'
+import Partners from '../components/Partners'
+import SpecialThanks from '../components/SpecialThanks'
 
 export default (props: {}) => (
-  <div>
+  <>
     {style}
-    <Jumbotron className='mb-0 bg-dark hero pad text-white'>
+    <Jumbotron id='#' className='mb-0 bg-dark hero pad text-white'>
       <Container>
         <Row className='my-5 d-flex align-items-center'>
           <Col xs={12} lg={7}>
-            <h2 className='header'>
-              We build software for charities
-            </h2>
+            <h2 className='header'>We build software for charities</h2>
             <p className='lead'>
               Coders for Causes is a not for profit organisation that empowers
-              charities and other not for profit organisations by connecting them
-              with university students to develop technical solutions. We are a
-              student-run club based in Perth, Western Australia with a wide range
-              of clients. Whether you are looking for technical advice or a long
-              term project, get in touch with us for more information.
+              charities and other not for profit organisations by connecting
+              them with university students to develop technical solutions. We
+              are a student-run club based in Perth, Western Australia with a
+              wide range of clients. Whether you are looking for technical
+              advice or a long term project, get in touch with us for more
+              information.
             </p>
           </Col>
         </Row>
       </Container>
     </Jumbotron>
 
-    <Container>
+    <Container id='team'>
       <Row className='pad'>
         <Col xs={12}>
-          <h3 className='header'>
-            Meet the Committee
-          </h3>
+          <h3 className='header'>Meet the Team</h3>
         </Col>
         <Col xs={12}>
           <CommitteeCard />
@@ -42,21 +43,37 @@ export default (props: {}) => (
       <Container>
         <Row>
           <Col xs={12}>
-            <h3 className='header'>
-              Our Clients
-            </h3>
+            <h3 className='header m-0'>Our Clients</h3>
           </Col>
-          <Col />
         </Row>
+        <Clients />
+        <Row>
+          <Col xs={12}>
+            <h3 className='header m-0'>Our Sponsors</h3>
+          </Col>
+        </Row>
+        <Sponsors />
+        <Row>
+          <Col xs={12}>
+            <h3 className='header m-0'>Our Partnered Clubs</h3>
+          </Col>
+        </Row>
+        <Partners />
+        <Row>
+          <Col xs={12}>
+            <h3 className='header m-0'>Special Thanks</h3>
+          </Col>
+        </Row>
+        <SpecialThanks />
       </Container>
     </Jumbotron>
-  </div>
+  </>
 )
 
 const style = (
   <style jsx>{`
     .header {
-      font-weight: bolder;
+      font-wzeight: bolder;
       margin-bottom: 1.2rem;
     }
     .pad {
@@ -66,9 +83,11 @@ const style = (
       margin-top: 4.5rem;
       border-radius: 0;
     }
-    .header-img {
-      max-width: 500px;
-      margin-top: 1rem;
+    .hero::after {
+      content: ' ';
+      left: 0;
+      background-color: yellow !important;
+      top: 4.5rem;
     }
   `}</style>
 )
