a475a1dab028c4684858d308ebe64f9d2c80b46b
Drewbi <22423862@student.uwa.edu.au>
2019-11-09 17:13:14 +0800

    fix(TypedText): Change block character to code representation
    Change block character to code representation

--- components/TypedText/index.tsx
+++ components/TypedText/index.tsx
@@ -11,7 +11,7 @@ export default ({ text }: { text: Array<string> }) => {
       typeSpeed: 50,
       backSpeed: 30,
       backDelay: 2000,
-      cursorChar: '∎',
+      cursorChar: '\u258A',
       loop: true
     })
 
