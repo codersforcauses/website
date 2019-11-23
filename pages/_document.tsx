71473de7c85e391545dbb9f9a3a77cfa6a8d69b8
jkkim <j.k.kim98@outlook.com>
2019-11-23 14:49:08 +0800

    add shortcut icon
    too small?

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,27 +8,28 @@ import Document, {
 import '../theme.scss'
 
 export default class MyDocument extends Document {
-  static async getInitialProps (ctx: DocumentContext) {
+  static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx)
     return { ...initialProps }
   }
 
-  render () {
+  render() {
     return (
       <html>
         <Head>
           {/* Typefaces from Google Fonts */}
           <link
-            href='https://fonts.googleapis.com/css?family=IBM+Plex+Mono|IBM+Plex+Sans'
-            rel='stylesheet'
+            href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono|IBM+Plex+Sans"
+            rel="stylesheet"
           />
           {/* Font Awesome */}
           <link
-            rel='stylesheet'
-            href='https://use.fontawesome.com/releases/v5.8.1/css/all.css'
-            integrity='sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf'
-            crossOrigin='anonymous'
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+            integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+            crossOrigin="anonymous"
           />
+          <link rel="shortcut icon" href="/logo/logo-01-100x100.png" />
         </Head>
         <body>
           <Main />
