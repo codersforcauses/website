524c0c0f393dae020c5add4a44966843fc4c618f
JKKim98 <j.k.kim98@outlook.com>
2019-12-07 13:21:25 +0800

    improvement(events_workshop.json): trim description

--- components/Events/EventCard/index.tsx
+++ components/Events/EventCard/index.tsx
@@ -19,22 +19,30 @@ type EventCardProps = {
   description: string
 }
 
-const EventCard = ({ id, img, title, description }: EventCardProps) => (
-  <>
-    {style}
-    <Card className="event-card">
-      <CardImg top src={img}></CardImg>
-      <CardBody>
-        <CardTitle>
-          <h1 className="title">{title}</h1>
-        </CardTitle>
-        <CardText className="description">{description}</CardText>
-        <Link href={'/event/' + id}>
-          <Button className="button">Learn more</Button>
-        </Link>
-      </CardBody>
-    </Card>
-  </>
-)
+const EventCard = ({ id, img, title, description }: EventCardProps) => {
+  const descriptionLength = 150
+  const descTrimmed =
+    description !== undefined && description.length > descriptionLength
+      ? description.substring(0, descriptionLength) + '...'
+      : description
+
+  return (
+    <>
+      {style}
+      <Card className='event-card'>
+        <CardImg top src={img} />
+        <CardBody>
+          <CardTitle>
+            <h1 className='title'>{title}</h1>
+          </CardTitle>
+          <CardText className='description'>{descTrimmed}</CardText>
+          <Link href={'/event/' + id}>
+            <Button className='button'>Learn more</Button>
+          </Link>
+        </CardBody>
+      </Card>
+    </>
+  )
+}
 
 export default EventCard
