b273d2b537b89f13ff581193f32485477f8e49e9
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2020-04-09 03:47:05 +0800

    update meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,6 +19,10 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
+          <meta
+            name='robots'
+            content='index, follow, max-snippet:30, max-image-preview:large'
+          />
           <link
             rel='preconnect'
             href='https://fonts.gstatic.com/'
@@ -34,11 +38,17 @@ class MyDocument extends Document {
             href='https://connect.facebook.net'
             crossOrigin=''
           />
+          <link
+            rel='preconnect'
+            href='https://scontent.fper5-1.fna.fbcdn.net'
+            crossOrigin=''
+          />
           <link rel='preconnect' href='https://unsplash.it' crossOrigin='' />
 
           <link rel='dns-prefetch' href='//fonts.gstatic.com/' />
           <link rel='dns-prefetch' href='//kit-free.fontawesome.com' />
           <link rel='dns-prefetch' href='//connect.facebook.net' />
+          <link rel='dns-prefetch' href='//scontent.fper5-1.fna.fbcdn.net' />
           <link rel='dns-prefetch' href='//unsplash.it' />
 
           {/* Typefaces from Google Fonts */}
