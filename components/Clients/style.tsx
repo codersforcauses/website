0452f36a48774528d1cadc95e7366e18abffedae
Drewbi <drewalexander986@gmail.com>
2020-04-14 02:18:49 +0800

    Darkened client logos

--- components/Clients/style.tsx
+++ components/Clients/style.tsx
@@ -6,7 +6,7 @@ export default (
       object-fit: contain;
       object-position: center;
       max-height: 100px;
-      filter: grayscale(100%) contrast(0.3) brightness(1.2);
+      filter: grayscale(100%) contrast(0.2) brightness(1.1);
     }
   `}
   </style>
