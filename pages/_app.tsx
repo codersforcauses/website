84d5818eb8f142609d537bb607ae5b7ea2b6027c
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2020-04-16 05:21:41 +0800

    Added AWS cognito signup using email

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,35 @@
 import App, { AppProps } from 'next/app'
 import { ThemeProvider } from 'emotion-theming'
 import React, { useEffect } from 'react'
+import Amplify from 'aws-amplify'
 import { initAnalytics } from '../helpers/analytics'
 import { initMessenger } from '../helpers/messenger'
 import { theme } from '../lib/theme'
 import '../theme.scss'
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+Amplify.configure({
+  aws_project_region: process.env.AMPLIFY_AWS_COGNITO_REGION,
+  aws_cognito_region: process.env.AMPLIFY_AWS_COGNITO_REGION,
+  aws_user_pools_id: process.env.AMPLIFY_AWS_COGNITO_USER_POOL_ID,
+  aws_user_pools_web_client_id: process.env.AMPLIFY_AWS_COGNITO_WEB_CLIENT_ID,
+  oauth: {
+    domain: process.env.AMPLIFY_OAUTH_DOMAIN,
+    scope: [
+      'phone',
+      'email',
+      'openid',
+      'profile',
+      'aws.cognito.signin.user.admin'
+    ],
+    redirectSignIn: `${process.env.BASE_URL}/signin/`,
+    redirectSignOut: process.env.BASE_URL,
+    responseType: 'token'
+  },
+  federationTarget: 'COGNITO_USER_POOLS',
+  authenticationFlowType: 'USER_PASSWORD_AUTH'
+})
+
+const Website = ({ Component, pageProps }: AppProps) => {
   useEffect(() => {
     if (process.env.NODE_ENV !== 'development') {
       initAnalytics('2423121134')
@@ -33,10 +56,10 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   )
 }
 
-MyApp.getInitialProps = async appContext => {
+Website.getInitialProps = async appContext => {
   const appProps = await App.getInitialProps(appContext)
 
   return { ...appProps }
 }
 
-export default MyApp
+export default Website
