2601e4a5dcb3f63975e623c2d5b17ae85fc704f2
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2020-04-19 18:30:59 +0800

    update now config

--- components/FindUs/Map/index.tsx
+++ components/FindUs/Map/index.tsx
@@ -1,10 +1,42 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
 import { withTheme } from 'emotion-theming'
-import { Head } from 'next/document'
-import { useState, useEffect, useRef } from 'react'
+import { createElement, useState, useEffect, useRef } from 'react'
 import mapboxgl from 'mapbox-gl'
 import { styles } from './styles'
+
+const property3D: mapboxgl.Layer = {
+  id: '3d-buildings',
+  source: 'composite',
+  'source-layer': 'building',
+  filter: ['==', 'extrude', 'true'],
+  type: 'fill-extrusion',
+  minzoom: 17,
+  paint: {
+    'fill-extrusion-color': '#333',
+    // use an 'interpolate' expression to add a smooth transition effect to the
+    // buildings as the user zooms in
+    'fill-extrusion-height': [
+      'interpolate',
+      ['linear'],
+      ['zoom'],
+      15,
+      0,
+      15.05,
+      ['get', 'height']
+    ],
+    'fill-extrusion-base': [
+      'interpolate',
+      ['linear'],
+      ['zoom'],
+      15,
+      0,
+      15.05,
+      ['get', 'min_height']
+    ],
+    'fill-extrusion-opacity': 0.6
+  }
+}
 
 const Map = (props: { marker: string; theme: Object }) => {
   const [map, setMap] = useState(null)
@@ -12,12 +44,15 @@ const Map = (props: { marker: string; theme: Object }) => {
 
   useEffect(() => {
     mapboxgl.accessToken = process.env.MAPBOX_API
+
+    const UWA_COORDS: [number, number] = [115.816986, -31.98097] // [lng, lat]
+
     const initializeMap = ({ setMap, mapContainer }) => {
       const map = new mapboxgl.Map({
         container: mapContainer.current,
         style: 'mapbox://styles/mapbox/light-v10',
-        center: [115.816986, -31.98097], // [lng, lat]
-        zoom: 15.5,
+        center: UWA_COORDS,
+        zoom: 9,
         pitch: 45,
         antialias: true
       })
@@ -27,62 +62,44 @@ const Map = (props: { marker: string; theme: Object }) => {
         map.resize()
 
         // 3-d buildings
-        const layers = map.getStyle().layers
         let labelLayerId
+        const layers = map.getStyle().layers
         for (let i = 0; i < layers.length; i++) {
           if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
             labelLayerId = layers[i].id
             break
           }
         }
-        map.addLayer(
-          {
-            id: '3d-buildings',
-            source: 'composite',
-            'source-layer': 'building',
-            filter: ['==', 'extrude', 'true'],
-            type: 'fill-extrusion',
-            minzoom: 16.5,
-            paint: {
-              'fill-extrusion-color': '#333',
-              // use an 'interpolate' expression to add a smooth transition effect to the
-              // buildings as the user zooms in
-              'fill-extrusion-height': [
-                'interpolate',
-                ['linear'],
-                ['zoom'],
-                15,
-                0,
-                15.05,
-                ['get', 'height']
-              ],
-              'fill-extrusion-base': [
-                'interpolate',
-                ['linear'],
-                ['zoom'],
-                15,
-                0,
-                15.05,
-                ['get', 'min_height']
-              ],
-              'fill-extrusion-opacity': 0.6
-            }
-          },
-          labelLayerId
-        )
+        map.addLayer(property3D, labelLayerId)
+
+        new mapboxgl.Marker({ color: '#000000' })
+          .setLngLat(UWA_COORDS)
+          .addTo(map)
+
+        // button for full screen map
+        map.addControl(new mapboxgl.FullscreenControl())
+
+        // fly to animation
+        setTimeout(() => {
+          map.flyTo({
+            zoom: 16,
+            speed: 0.6, // make the flying slow
+
+            // This can be any easing function: it takes a number between
+            // 0 and 1 and returns another number between 0 and 1.
+            easing: t => t,
+
+            // this animation is considered essential with respect to prefers-reduced-motion
+            essential: true
+          })
+        }, 1000)
       })
 
       // add marker for clubroom location
-      const el = document.createElement('div')
-      el.className = 'marker'
-      new mapboxgl.Marker(el).setLngLat([115.816986, -31.98097]).addTo(map)
-
-      // button for full screen map
-      map.addControl(new mapboxgl.FullscreenControl())
     }
 
     if (!map) initializeMap({ setMap, mapContainer })
-  }, [map])
+  }, [map, property3D])
 
   return (
     <div
