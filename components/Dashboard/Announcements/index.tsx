cc6c4e708adf5d9a732f0edf4d0e815481d4de2a
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2020-05-29 03:10:48 +0800

    Annoucements init

---
+++ components/Dashboard/Announcements/index.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+// import { ListGroup, ListGroupItem } from 'reactstrap'
+import { Alert } from 'reactstrap'
+import announcements from 'data/announcements.json'
+
+// const Announcement = (props: { text: string; time: string }) => {
+//   return (
+//     <ListGroupItem className='d-flex flex-column rounded-0 px-0'>
+//       <p className='m-0'>{props.text}</p>
+//       <small className='align-self-end text-muted'>{props.time}</small>
+//     </ListGroupItem>
+//   )
+// }
+
+// const Announcements = () => {
+//   return (
+//     <ListGroup flush>
+//       {announcements.length > 0 ? (
+//         announcements.map(data => (
+//           <Announcement text={data.text} time={data.time} />
+//         ))
+//       ) : (
+//         <p className='m-0 text-muted'>No announcements to show</p>
+//       )}
+//     </ListGroup>
+//   )
+// }
+
+const Announcement = (props: { text: string; time: string }) => {
+  const [visible, setVisible] = useState(true)
+
+  const onDismiss = () => setVisible(false)
+
+  const colours = [
+    'primary',
+    'secondary',
+    'accent',
+    'success',
+    'danger',
+    'warning',
+    'light'
+  ]
+  const randomColour = colours[Math.floor(Math.random() * colours.length)]
+  return (
+    <Alert
+      color={randomColour}
+      isOpen={visible}
+      toggle={onDismiss}
+      className={`d-flex flex-column rounded-0 pr-5 border border-${
+        randomColour === 'light' || randomColour === 'secondary'
+          ? 'muted'
+          : randomColour
+      }`}
+    >
+      <p className='m-0'>{props.text}</p>
+      <small className='align-self-end' style={{ opacity: 0.6 }}>
+        {props.time}
+      </small>
+    </Alert>
+  )
+}
+
+const Announcements = () => {
+  return (
+    <>
+      {announcements.length > 0 ? (
+        announcements.map((data, i) => (
+          <Announcement key={i} text={data.text} time={data.time} />
+        ))
+      ) : (
+        <p className='m-0 text-muted'>No announcements to show</p>
+      )}
+    </>
+  )
+}
+
+export default Announcements
--- components/Dashboard/DashboardPage/index.tsx
+++ components/Dashboard/DashboardPage/index.tsx
@@ -3,12 +3,22 @@ import { jsx } from '@emotion/core'
 import { withTheme } from 'emotion-theming'
 import { Jumbotron, Container, Row, Col, Card, Input } from 'reactstrap'
 import Title from 'components/Utils/Title'
+import Announcements from '../Announcements'
 import { styles } from './styles'
 
 const BrandPage = (props: { theme: Object }) => {
   return (
     <div css={styles(props.theme)}>
       <Title typed>./dashboard</Title>
+      <Container className='py-5'>
+        <Row>
+          <Col md={8} />
+          <Col md={4}>
+            <h6 className='font-weight-bold mb-3'>Announcements</h6>
+            <Announcements />
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
