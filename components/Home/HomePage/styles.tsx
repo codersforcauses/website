87ddf6ac4b26739d5b328f0135a96765c58076aa
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-06-17 19:41:44 +0800

    Fixed line spacing issue on high resolution screen

--- components/Home/HomePage/styles.tsx
+++ components/Home/HomePage/styles.tsx
@@ -9,7 +9,8 @@ export const styles = theme => css`
 
   .lead {
     /* font-size: calc([minimum size] + ([maximum size] - [minimum size]) * ((100vw - [minimum viewport width]) / ([maximum viewport width] - [minimum viewport width]))); */
-   font-size: calc(14px + (26 - 14) * ((100vw - 300px) / (1600 - 300)));
+    font-size: calc(10px + (26 - 14) * ((100vw - 300px) / (1600 - 300)));
+    line-height: calc(1.3em + (1.5 - 1.2) * ((100vw - 300px)/(1600 - 300)));
     }
 
   .call {
