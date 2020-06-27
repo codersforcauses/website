e27f9b87344061c66dab2aba45aeb6355326acf8
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-06-27 14:05:00 +0800

    OG image for about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -10,6 +10,10 @@ const About = () => (
         name='description'
         content='Coders for Causes is a not for profit organisation that empowers charities and other not for profit organisations by connecting them with university students to develop technical solutions. We are a student-run club based in Perth, Western Australia with a wide range of clients. Whether you are looking for technical advice or a long term project, get in touch with us for more information.'
       />
+      <meta
+        property='og:image'
+        content='https://og-social-cards.dankestkush.vercel.app/**.%2Fabout**.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fwebsite.codersforcauses.org%2Flogo%2Fcfc_logo_white_full.svg'
+      />
       <link rel='preconnect' href='https://api.mapbox.com' crossOrigin='' />
       <link rel='dns-prefetch' href='//api.mapbox.com' />
 
