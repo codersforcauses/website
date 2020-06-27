8c030bb6f0f25d340e6a664e19be473f5a8d7806
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-06-27 13:39:35 +0800

    OG image for dashboard page

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -18,6 +18,10 @@ const Dashboard = () => {
           name='description'
           content='Coders for Causes is a not for profit organisation that empowers charities and other not for profit organisations by connecting them with university students to develop technical solutions. We are a student-run club based in Perth, Western Australia with a wide range of clients. Whether you are looking for technical advice or a long term project, get in touch with us for more information.'
         />
+        <meta
+          property='og:image'
+          content='hhttps://og-social-cards.dankestkush.vercel.app/**.%2Fdashboard**.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fwebsite.codersforcauses.org%2Flogo%2Fcfc_logo_white_full.svg'
+        />
       </Head>
       <DashboardPage />
     </>
