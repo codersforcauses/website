6b5376a906371bc5e28eef2c76492a9504d25a00
MouseAndKeyboard <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-07-24 14:28:07 +0800

    removed old test code comment

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -14,32 +14,3 @@ describe('Test desktop element visibility', () => {
       .contains(';)')
   })
 })
-
-// const email = fake.internet.email()
-  // const pass = fake.internet.password(8)
-  // beforeEach(() => {
-  //   cy.clearLocalStorage()
-  //   cy.visit('/login')
-  // })
-  // it('Visits the Login page and finds the p2s logo', () => {
-  //   cy.get('div.v-image')
-  //     .should('be.visible')
-  //     .should('have.css', 'background-image')
-  // })
-  // it('Visits the Login page and finds the login form', () => {
-  //   cy.get('label').should('be.visible').contains('EMAIL')
-  //   cy.get('[name="email"]').should('be.visible')
-  //   cy.get('label').should('be.visible').contains('PASSWORD')
-  //   cy.get('[name="password"]').should('be.visible')
-  //   cy.get('button[name="login"]').should('be.disabled')
-  //   cy.get('button[name="forgotPass"]').should('be.visible')
-  // })
-  // it('Logs user in, fails', () => {
-  //   cy.login(email, pass)
-  //   cy.wait(5000)
-  //   cy.get('div[name="alert"]').contains('Error:').should('be.visible')
-  // })
-  // // Fails until connected to the backend
-  // // it('Logs user in, pass', () => {
-  // //   cy.login();
-  // // });
