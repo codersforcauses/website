2ed4ac4c5a6a67ce8aaf9496f17b178e82501a2a
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-07-24 11:06:37 +0800

    Refactor: cypress mobile nav tests

--- cypress/integration/header.spec.ts
+++ cypress/integration/header.spec.ts
@@ -1,13 +1,20 @@
 /// <reference path="../support/index.d.ts" />
 
-const testNavButton = (buttonId: string, title: string, url: string) => {
+const testNavButton = (buttonId: string, url: string) => {
   cy.get(`[data-cy=nav-${buttonId}]`)
     .click()
   cy.url().should('eq', Cypress.config().baseUrl + `${url}`);
-  cy.get('[data-cy=typer]')
-        .contains(`./${title}`)
-
 }
+
+const createNavTest = (msg: string, buttonId: string, url: string) => () => it(msg, () => { testNavButton(buttonId, url) })
+
+const navTest = [
+  createNavTest('should be able to navigate home page', 'Home', '/'),
+  createNavTest('should be able to navigate to /about', 'About', '/about'),
+  createNavTest('should be able to navigate to /events', 'Events', '/events'),
+  createNavTest('should be able to navigate to /projects', 'Projects', '/projects')
+]
+
 
 describe('Test Desktop Navbar', () => {
   beforeEach(() => {
@@ -15,21 +22,8 @@ describe('Test Desktop Navbar', () => {
     cy.visit('/')
   })
 
-  it('should be able to navigate home page', () => {
-    testNavButton("Home", "Innovation with a mission", '/');
-  })
+  navTest.forEach( (test) => test() )
 
-  it('should be able to navigate to /about', () => {
-    testNavButton("About", "about", '/about')
-  })
-
-  it('should be able to navigate to /events', () => {
-    testNavButton("Events", "events", '/events')
-  })
-
-  it('should be able to navigate to /projects', () => {
-    testNavButton("Projects", "projects", '/projects')
-  })
 })
 
 describe('Test Mobile Navbar', () => {
@@ -40,23 +34,6 @@ describe('Test Mobile Navbar', () => {
     cy.get('[id="Menu"]').click()
   })
 
-  it('should be able to navigate home page', () => {
-    cy.get('[id="Home"]').click()
-    cy.contains('./Innovation with a mission')
-  })
+  navTest.forEach( (test) => test() )
 
-  it('should be able to navigate to /about', () => {
-    cy.get('[data-tid="nav-About"]').click()
-    cy.contains('./about')
-  })
-
-  it('should be able to navigate to /events', () => {
-    cy.get('[data-tid="nav-Events"]').click()
-    cy.contains('./events')
-  })
-
-  it('should be able to navigate to /projects', () => {
-    cy.get('[data-tid="nav-Projects"]').click()
-    cy.contains('./projects')
-  })
 })
