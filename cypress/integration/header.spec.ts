a704b50b131c0c7e2f1bc6d4e9854fb1db5207ae
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-07-25 07:19:47 +0800

    Refactored nav tests to be clearer

--- cypress/integration/header.spec.ts
+++ cypress/integration/header.spec.ts
@@ -6,23 +6,21 @@ const testNavButton = (buttonId: string, url: string) => {
   cy.url().should('eq', Cypress.config().baseUrl + `${url}`);
 }
 
-const createNavTest = (msg: string, buttonId: string, url: string) =>
-  () => it(msg, () => { testNavButton(buttonId, url) })
+const createNavTest = (msg: string, buttonId: string, url: string) => it(msg, () => { testNavButton(buttonId, url) })
 
-const navTest = [
-  createNavTest('should be able to navigate home page', 'Home', '/'),
-  createNavTest('should be able to navigate to /about', 'About', '/about'),
-  createNavTest('should be able to navigate to /events', 'Events', '/events'),
+const navigationTests = () => {
+  createNavTest('should be able to navigate home page', 'Home', '/')
+  createNavTest('should be able to navigate to /about', 'About', '/about')
+  createNavTest('should be able to navigate to /events', 'Events', '/events')
   createNavTest('should be able to navigate to /projects', 'Projects', '/projects')
-]
-
+}
 
 describe('Test Desktop Navbar', () => {
   beforeEach(() => {
     cy.visit('/')
   })
 
-  navTest.forEach( (test) => test() )
+  navigationTests()
 
 })
 
@@ -33,6 +31,6 @@ describe('Test Mobile Navbar', () => {
     cy.get('[id="Menu"]').click()
   })
 
-  navTest.forEach( (test) => test() )
+  navigationTests()
 
 })
