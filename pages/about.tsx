fa317c025ec7d15c925886d03822b995d80315a5
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-07-24 19:39:48 +0800

    Added seo component to about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,24 +1,17 @@
 import React from 'react'
 import Head from 'next/head'
 import AboutPage from 'components/About/AboutPage'
+import Seo from 'components/Utils/SEO'
 
 const About = () => (
   <>
     <Head>
       <title>About | Coders for Causes</title>
-      <meta
-        name='description'
-        content='Coders for Causes is a not for profit organisation that empowers charities and other not for profit organisations by connecting them with university students to develop technical solutions. We are a student-run club based in Perth, Western Australia with a wide range of clients. Whether you are looking for technical advice or a long term project, get in touch with us for more information.'
+      <Seo
+        title='About Us'
+        description='A group of students who develop technical solutions for charities and other not for profit organisations.'
+        image='https://og-social-cards.dankestkush.vercel.app/**.%2Fabout**.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fwebsite.codersforcauses.org%2Flogo%2Fcfc_logo_white_full.svg'
       />
-      <meta
-        property='og:image'
-        content='https://og-social-cards.dankestkush.vercel.app/**.%2Fabout**.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fwebsite.codersforcauses.org%2Flogo%2Fcfc_logo_white_full.svg'
-      />
-      <meta property='og:site_name' content='Coders for Causes' />
-
-      <meta property='og:title' content='About Us' />
-      <meta property='og:description' content='A group of students who develop technical solutions for charities and other not for profit organisations.' />
-
       <link rel='preconnect' href='https://api.mapbox.com' crossOrigin='' />
       <link rel='dns-prefetch' href='//api.mapbox.com' />
 
