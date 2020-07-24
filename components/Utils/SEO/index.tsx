d979416f8f8721531a6c1b5bf2d4d6a0ee463e07
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-07-24 21:24:07 +0800

    copied dan abramov's OG meta tag formatting

--- components/Utils/SEO/index.tsx
+++ components/Utils/SEO/index.tsx
@@ -9,13 +9,16 @@ type SEOProps = {
 const SEO = ({ title, image, description }: SEOProps) => {
   return (
     <>
-      <meta property='og:image' content={image} />
-      <meta property='og:site_name' content='Coders for Causes' />
+      <meta name='description' content={description} />
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:creator' content='@codersforcauses' />
+      <meta property='og:url' content='https://codersforcauses.org' />
       <meta property='og:title' content={title} />
       <meta property='og:description' content={description} />
-      <meta name='description' content={description} />
-      <meta name='twitter:card' content='summary' />
-      <meta name='twitter:site' content='@codersforcauses' />
+      <meta property='og:image' content={image} />
+      <meta property='og:image:width' content='512' />
+      <meta property='og:image:height' content='293' />
+      <meta property='og:site_name' content='Coders for Causes' />
       <meta name='twitter:title' content={title} />
       <meta name='twitter:description' content={description} />
       <meta name='twitter:image' content={image} />
