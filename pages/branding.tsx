b14d4ecded5beb0915c93c6342dc976f7cc0c04d
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-07-24 19:45:50 +0800

    seo component to branding page

--- pages/branding.tsx
+++ pages/branding.tsx
@@ -1,23 +1,17 @@
 import React from 'react'
 import Head from 'next/head'
+import Seo from 'components/Utils/SEO'
 import BrandPage from 'components/Brand/BrandPage'
 
 const Branding = () => (
   <>
     <Head>
       <title>Branding | Coders for Causes</title>
-      <meta
-        name='description'
-        content='Coders for Causes branding. Looking to use our logos, fonts, or colours for something? Please follow the guidelines laid out.'
+      <Seo
+        title='Branding'
+        description='Logos, colour schemes, icons and more...'
+        image='https://og-social-cards.dankestkush.vercel.app/**.%2Fbranding**.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fwebsite.codersforcauses.org%2Flogo%2Fcfc_logo_white_full.svg'
       />
-      <meta
-        property='og:image'
-        content='https://og-social-cards.dankestkush.vercel.app/**.%2Fbranding**.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fwebsite.codersforcauses.org%2Flogo%2Fcfc_logo_white_full.svg'
-      />
-      <meta property='og:site_name' content='Coders for Causes' />
-      <meta property='og:title' content='Branding' />
-      <meta property='og:description' content='Logos, colour schemes, icons and more...' />
-
     </Head>
 
     <BrandPage />
