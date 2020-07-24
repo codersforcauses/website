36bf1956bd0563a8c4e43f4fb1b9cd086c12254a
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-07-24 13:43:05 +0800

    OG improved homepage image

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ const index = () => (
       />
       <meta
         property='og:image'
-        content='https://og-social-cards.dankestkush.vercel.app/**.%2F**.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fwebsite.codersforcauses.org%2Flogo%2Fcfc_logo_white_full.svg'
+        content='https://og-social-cards.dankestkush.vercel.app/**.%2FInnovation%20with%20a%20mission**.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fwebsite.codersforcauses.org%2Flogo%2Fcfc_logo_white_full.svg'
       />
       <meta property='og:site_name' content='Coders for Causes' />
     </Head>
