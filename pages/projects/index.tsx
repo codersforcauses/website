16f741077db17e98dc4c89de4a12030fb1838fbf
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-07-24 20:09:16 +0800

    seo component to projects page

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,22 +1,17 @@
 import React from 'react'
 import Head from 'next/head'
+import Seo from 'components/Utils/SEO'
 import ProjectsPage from 'components/Projects/ProjectsPage'
 
 const Projects = () => (
   <>
     <Head>
       <title>Projects | Coders for Causes</title>
-      <meta
-        name='description'
-        content='Coders for Causes is a not for profit organisation that empowers charities and other not for profit organisations by connecting them with university students to develop technical solutions. We are a student-run club based in Perth, Western Australia with a wide range of clients. Whether you are looking for technical advice or a long term project, get in touch with us for more information.'
+      <Seo
+        title='Our Projects'
+        description='A list of all past and present projects.'
+        image='https://og-social-cards.dankestkush.vercel.app/**.%2Fprojects**.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fwebsite.codersforcauses.org%2Flogo%2Fcfc_logo_white_full.svg'
       />
-      <meta
-        property='og:image'
-        content='https://og-social-cards.dankestkush.vercel.app/**.%2Fprojects**.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fwebsite.codersforcauses.org%2Flogo%2Fcfc_logo_white_full.svg'
-      />
-      <meta property='og:title' content='Our projects' />
-      <meta property='og:description' content='A list of all past and present projects.' />
-      <meta property='og:site_name' content='Coders for Causes' />
     </Head>
     <ProjectsPage />
   </>
