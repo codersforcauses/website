08c27130456833f55e26d350056d2ab9dbbd46ae
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-07-24 20:03:59 +0800

    seo component to homepage

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react'
 import Router from 'next/router'
 import Head from 'next/head'
+import Seo from 'components/Utils/SEO'
 import DashboardPage from 'components/Dashboard/DashboardPage'
 import { UserContext } from 'helpers/user'
 
@@ -14,18 +15,11 @@ const Dashboard = () => {
     <>
       <Head>
         <title>Dashboard | Coders for Causes</title>
-        <meta
-          name='description'
-          content='Coders for Causes is a not for profit organisation that empowers charities and other not for profit organisations by connecting them with university students to develop technical solutions. We are a student-run club based in Perth, Western Australia with a wide range of clients. Whether you are looking for technical advice or a long term project, get in touch with us for more information.'
+        <Seo
+          title='Dashboard'
+          description='Get updates and access to CFC material.'
+          image='https://og-social-cards.dankestkush.vercel.app/**.%2Fdashboard**.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fwebsite.codersforcauses.org%2Flogo%2Fcfc_logo_white_full.svg'
         />
-        <meta
-          property='og:image'
-          content='hhttps://og-social-cards.dankestkush.vercel.app/**.%2Fdashboard**.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fwebsite.codersforcauses.org%2Flogo%2Fcfc_logo_white_full.svg'
-        />
-        <meta property='og:title' content='Dashboard' />
-        <meta property='og:description' content='Get updates and access to CFC material.' />
-
-        <meta property='og:site_name' content='Coders for Causes' />
       </Head>
       <DashboardPage />
     </>
