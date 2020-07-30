a34191dd4dcfa14402a7d4c7786a3fdf7d3bec0e
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-07-31 05:55:43 +0800

    Removed createNavTest function

--- cypress/integration/header.spec.ts
+++ cypress/integration/header.spec.ts
@@ -5,20 +5,11 @@ const testNavButton = (buttonId: string, url: string) => {
   cy.url().should('eq', Cypress.config().baseUrl + `${url}`)
 }
 
-const createNavTest = (msg: string, buttonId: string, url: string) =>
-  it(msg, () => {
-    testNavButton(buttonId, url)
-  })
-
 const navigationTests = () => {
-  createNavTest('should be able to navigate home page', 'Home', '/')
-  createNavTest('should be able to navigate to /about', 'About', '/about')
-  createNavTest('should be able to navigate to /events', 'Events', '/events')
-  createNavTest(
-    'should be able to navigate to /projects',
-    'Projects',
-    '/projects'
-  )
+  it('should be able to navigate home page', () => testNavButton('Home', '/'))
+  it('should be able to navigate to /about', () => testNavButton('About', '/about'))
+  it('should be able to navigate to /events', () => testNavButton('Events', '/events'))
+  it('should be able to navigate to /projects', () => testNavButton( 'Projects', '/projects'))
 }
 
 describe('Test Desktop Navbar', () => {
