300df43898ba9359483f9b2d1eb1dada1e233976
Drewbi <drewalexander986@gmail.com>
2020-09-01 15:49:49 +0800

    Type changes

--- components/Projects/ProjectPage/TechList/index.tsx
+++ components/Projects/ProjectPage/TechList/index.tsx
@@ -8,7 +8,7 @@ const TechList = ({ data }: Props) => {
   console.log(data)
   return (
     <>
-      {data.map((tech: String, i: Number) => (
+      {data.map((tech: any, i: number) => (
         <div key={i} className='d-flex align-items-center monospace mb-3' css={styles(theme)}>
           <div className='bg-primary mr-3 bigger'>
             <i className='material-icons-sharp text-secondary p-2 bigger'>menu</i>
