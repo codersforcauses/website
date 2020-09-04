7877871c10aa4f83ea8f52d13c9b0ad26c849b6c
Drewbi <drewalexander986@gmail.com>
2020-09-04 17:59:05 +0800

    Add custom icons

--- components/Projects/ProjectPage/TechList/index.tsx
+++ components/Projects/ProjectPage/TechList/index.tsx
@@ -1,6 +1,8 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
 import { useTheme } from 'emotion-theming'
+import BrandIcon from 'components/Elements/BrandIcons'
+import type { Brand } from 'components/Elements/BrandIcons'
 import { styles } from './styles'
 
 const TechList = ({ data }: Props) => {
@@ -9,8 +11,8 @@ const TechList = ({ data }: Props) => {
     <>
       {data.map((tech: Tech) => (
         <div key={tech.name} className='d-flex align-items-center monospace mb-3' css={styles(theme)}>
-          <div className='bg-primary mr-3 bigger'>
-            <i className='material-icons-sharp text-secondary p-2 bigger'>{tech.icon}</i>
+          <div className='bg-primary mr-3 p-2'>
+            <BrandIcon icon={tech.icon} dimensions={32} fill='secondary' />
           </div>
           {tech.name}
         </div>
@@ -21,7 +23,7 @@ const TechList = ({ data }: Props) => {
 
 interface Tech {
   name: string,
-  icon: string
+  icon: Brand
 }
 interface Props {
   data: Array<Tech>
