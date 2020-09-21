65da00d19d27476171e7c3babb360cbb741a88b2
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2020-09-22 01:04:06 +0800

    feat(validation): update validation with schema limits

--- components/Auth/SignUpPage/Step1/OtherMember/validation.tsx
+++ components/Auth/SignUpPage/Step1/OtherMember/validation.tsx
@@ -1,10 +1,15 @@
 import { object, string } from 'yup'
 
 export const validationSchema = object().shape({
-  firstName: string().required('Please enter your first name'),
-  lastName: string().required('Please enter your last name'),
+  firstName: string()
+    .max(64, 'Our first name limit is 64 characters')
+    .required('Please enter your first name'),
+  lastName: string()
+    .max(64, 'Our last name limit is 64 characters')
+    .required('Please enter your last name'),
   email: string()
     .required('Please enter your email')
+    .max(64, 'Our email limit is 128 characters')
     .email('You must enter a valid email address'),
   password: string()
     .required('Please enter a password')
