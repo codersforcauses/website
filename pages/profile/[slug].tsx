11c93f1df1957475287c133a3d7783bfa26ec9c7
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2020-10-03 18:57:32 +0800

    chore(log): remove log statements

--- pages/profile/[slug].tsx
+++ pages/profile/[slug].tsx
@@ -4,24 +4,20 @@ import Seo from 'components/Utils/SEO'
 import ProfilePage from 'components/Auth/ProfilePage'
 import { User } from 'helpers/user'
 
-const Profile = ({ user }: { user: User }) => {
-  console.log(user)
-
-  return (
-    <>
-      <Head>
-        <title>{user?.name}'s Profile | Coders for Causes</title>
-        <Seo
-          title='Edit your account'
-          page='account_settings'
-          description='Customise your personal preferences and settings'
-          image='https://og-social-cards.vercel.app/**.%2Fsettings**.png?theme=dark&md=1&fontSize=125px&images=https%3A%2Fcodersforcauses.org%2Flogo%2Fcfc_logo_white_full.svg'
-        />
-      </Head>
-      <ProfilePage user={user} />
-    </>
-  )
-}
+const Profile = ({ user }: { user: User }) => (
+  <>
+    <Head>
+      <title>{user?.name}'s Profile | Coders for Causes</title>
+      <Seo
+        title='Edit your account'
+        page='account_settings'
+        description='Customise your personal preferences and settings'
+        image='https://og-social-cards.vercel.app/**.%2Fsettings**.png?theme=dark&md=1&fontSize=125px&images=https%3A%2Fcodersforcauses.org%2Flogo%2Fcfc_logo_white_full.svg'
+      />
+    </Head>
+    <ProfilePage user={user} />
+  </>
+)
 
 export const getServerSideProps = async ({ params }) => {
   const res = await fetch(
