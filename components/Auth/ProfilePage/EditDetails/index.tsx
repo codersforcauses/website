773e6e9bb2e8169917056ed8632f06ed64b0f4ba
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2020-10-05 16:00:36 +0800

    feat(dob): fix ts error

--- components/Auth/ProfilePage/EditDetails/index.tsx
+++ components/Auth/ProfilePage/EditDetails/index.tsx
@@ -15,7 +15,7 @@ import {
 } from 'reactstrap'
 import dayjs from 'dayjs'
 import localeData from 'dayjs/plugin/localeData'
-import { DarkContext } from 'helpers/user'
+import { DarkContext, User } from 'helpers/user'
 import { validationSchema } from './validation'
 
 dayjs.extend(localeData)
@@ -288,55 +288,12 @@ interface FormValues {
   isGuildMember: boolean
   gender: 'male' | 'female' | 'other'
   dob: {
-    day:
-      | 1
-      | 2
-      | 3
-      | 4
-      | 5
-      | 6
-      | 7
-      | 8
-      | 9
-      | 10
-      | 11
-      | 12
-      | 13
-      | 14
-      | 15
-      | 16
-      | 17
-      | 18
-      | 19
-      | 20
-      | 21
-      | 22
-      | 23
-      | 24
-      | 25
-      | 26
-      | 27
-      | 28
-      | 29
-      | 30
-      | 31
-    month:
-      | 'January'
-      | 'February'
-      | 'March'
-      | 'April'
-      | 'May'
-      | 'June'
-      | 'July'
-      | 'August'
-      | 'September'
-      | 'October'
-      | 'November'
-      | 'December'
+    day: number
+    month: string
     year: number
   }
 }
 interface Props {
-  user: any
+  user: User
   handleCancel: Function
 }
