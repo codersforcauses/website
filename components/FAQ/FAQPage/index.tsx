9a494cfb1aec42921d938860239ba51eae0f7d6b
Daphne Yu <49707328+definitelyDaphne@users.noreply.github.com>
2020-10-29 18:06:12 +0800

    Update question - stack_choice
    Co-authored-by: Drew Alexander <drewalexander986@gmail.com>

--- components/FAQ/FAQPage/index.tsx
+++ components/FAQ/FAQPage/index.tsx
@@ -32,7 +32,7 @@ const questionList = [
     {
         id:'stack_choice',
         description:'What stack do you normally use for your projects?',
-        answer:'We work with Javascript, Typescript and Node. Framework will most likely be Vue and Nuxt.js with express, or React with Next.js'
+        answer:'We work with Javascript, Typescript and Node. Frameworks will likely be either Vue with Nuxt.js or React with Next.js and an Express backend.'
     },
     {
         id:'inexperienced_newbie',
