f50b11bc4d68253a1a7d937be0542693feae8ac0
Drewbi <drewalexander986@gmail.com>
2020-10-29 11:20:48 +0800

    Ternary format

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,21 +29,23 @@ Auth.configure({
 const AddOns = () => {
   const { user } = useContext(UserContext)
 
-  return process.env.NODE_ENV === 'production' ? (
-    <>
-      <div id='fb-root' />
-      <div
-        className='fb-customerchat'
-        data-theme_color='#000000'
-        data-page_id='700598980115471'
-        data-logged_in_greeting={`Hi ${
-          user?.given_name ?? 'there'
-        }! How can we help you?`}
-        data-logged_out_greeting='Please log into facebook to chat with us'
-        data-greeting_dialog_display='fade'
-      />
-    </>
-  ) : null
+  return process.env.NODE_ENV === 'production'
+    ? (
+      <>
+        <div id='fb-root' />
+        <div
+          className='fb-customerchat'
+          data-theme_color='#000000'
+          data-page_id='700598980115471'
+          data-logged_in_greeting={`Hi ${
+            user?.given_name ?? 'there'
+          }! How can we help you?`}
+          data-logged_out_greeting='Please log into facebook to chat with us'
+          data-greeting_dialog_display='fade'
+        />
+      </>
+      )
+    : null
 }
 
 const Website = ({ Component, pageProps }: AppProps) => {
