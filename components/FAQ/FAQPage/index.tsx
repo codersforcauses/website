33eb8fd51d52d5ee5f331e23e66afd91bf7a5405
Daphne Yu <49707328+definitelyDaphne@users.noreply.github.com>
2020-10-29 18:08:17 +0800

    Update question - inexperienced_newbie
    Co-authored-by: Drew Alexander <drewalexander986@gmail.com>

--- components/FAQ/FAQPage/index.tsx
+++ components/FAQ/FAQPage/index.tsx
@@ -37,7 +37,7 @@ const questionList = [
     {
         id:'inexperienced_newbie',
         description:'Should I apply if I don\'t have any experience with HTML/CSS/Javascript?',
-        answer: 'It\'s strongly recommended but not necessary. The reason for that is we work with technologies that build on top of those basic language. Without knowing the basic, it might be hard to grasp the concept. If you would like some guidance for learning, feel free to head to #coding-resources section in our Discord server! '
+        answer: 'It\'s strongly recommended but not necessary. The reason for that is we work with technologies that build on top of those basic fundamentals. Without knowing the basics, it might be hard to grasp more difficult concepts. If you would like some guidance on where to start, head to the #coding-resources section in our Discord server! '
     },
     {
         id:'hour_committed',
