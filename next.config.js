679e933cf476d7b37aa7d46076b279593ad797ec
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2021-03-31 04:33:10 +0800

    trying css optimisation

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 module.exports = withBundleAnalyzer({
-  future: {
-    webpack5: true
-  }
+  future: { webpack5: true },
+  experimental: { optimizeCss: true }
 })
