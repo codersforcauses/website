cf4da8987f8eddfaf673dcae25559a46c4096895
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2021-03-31 07:04:46 +0800

    unify prod check for add-ons

--- components/Utils/AddOns/index.tsx
+++ components/Utils/AddOns/index.tsx
@@ -3,34 +3,31 @@ import { UserContext } from 'helpers/user'
 import { initMessenger } from 'helpers/messenger'
 
 const AddOns = () => {
-  const { user } = useContext(UserContext)
+  if (process.env.NODE_ENV !== 'production') return null
 
+  const { user } = useContext(UserContext)
   useEffect(() => {
-    if (process.env.NODE_ENV === 'production') {
-      initMessenger()
-      setTimeout(() => {
-        document.getElementsByTagName('iframe')[0].title =
-          'Coders for Causes FaceBook Messenger Plugin'
-      }, 2000)
-    }
+    initMessenger()
+    setTimeout(() => {
+      document.getElementsByTagName('iframe')[0].title =
+        'Coders for Causes FaceBook Messenger Plugin'
+    }, 2000)
   }, [])
 
   return (
-    process.env.NODE_ENV === 'production' && (
-      <>
-        <div id='fb-root' />
-        <div
-          className='fb-customerchat'
-          data-theme_color='#000000'
-          data-page_id='700598980115471'
-          data-logged_in_greeting={`Hi ${
-            user?.given_name ?? 'there'
-          }! How can we help you?`}
-          data-logged_out_greeting='Please log into facebook to chat with us'
-          data-greeting_dialog_display='fade'
-        />
-      </>
-    )
+    <>
+      <div id='fb-root' />
+      <div
+        className='fb-customerchat'
+        data-theme_color='#000000'
+        data-page_id='700598980115471'
+        data-logged_in_greeting={`Hi ${
+          user?.given_name ?? 'there'
+        }! How can we help you?`}
+        data-logged_out_greeting='Please log into facebook to chat with us'
+        data-greeting_dialog_display='fade'
+      />
+    </>
   )
 }
 
