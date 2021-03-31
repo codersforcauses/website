f156378cdd73745f0e7bcd0c7d67f9159ae90bfc
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2021-03-31 18:55:12 +0800

    update messenger plugin

--- components/Utils/AddOns/index.tsx
+++ components/Utils/AddOns/index.tsx
@@ -1,6 +1,20 @@
 import { useContext, useEffect } from 'react'
 import { UserContext } from 'helpers/user'
-import { initMessenger } from 'helpers/messenger'
+
+const initMessenger = () => {
+  const widgetInit = (d, s, id) => {
+    if (d.getElementById(id)) return
+    const fjs = d.getElementsByTagName(s)[0]
+    const js = d.createElement(s)
+    js.id = id
+    js.src =
+      'https://connect.facebook.net/en_GB/sdk/xfbml.customerchat.js#xfbml=1&version=v10.0&autoLogAppEvents=1'
+    fjs.parentNode.insertBefore(js, fjs)
+  }
+  setTimeout(() => {
+    widgetInit(document, 'script', 'facebook-jssdk')
+  }, 4000)
+}
 
 const AddOns = () => {
   if (process.env.NODE_ENV !== 'production') return null
@@ -8,10 +22,10 @@ const AddOns = () => {
   const { user } = useContext(UserContext)
   useEffect(() => {
     initMessenger()
-    setTimeout(() => {
-      document.getElementsByTagName('iframe')[0].title =
-        'Coders for Causes FaceBook Messenger Plugin'
-    }, 2000)
+    // setTimeout(() => {
+    //   document.getElementsByTagName('iframe')[0].title =
+    //     'Coders for Causes FaceBook Messenger Plugin'
+    // }, 2000)
   }, [])
 
   return (
@@ -19,6 +33,7 @@ const AddOns = () => {
       <div id='fb-root' />
       <div
         className='fb-customerchat'
+        data-attribution='setup_tool'
         data-theme_color='#000000'
         data-page_id='700598980115471'
         data-logged_in_greeting={`Hi ${
