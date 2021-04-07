9a53b6e22994c2ebb250286d3d409d020b89efbd
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2021-04-08 03:41:08 +0800

    fix breaking build

--- lib/mediaQueries.ts
+++ lib/mediaQueries.ts
@@ -1,15 +1,17 @@
 import { css } from '@emotion/react'
-import { theme } from './theme'
+import { theme, Breakpoints } from './theme'
+
+type Media = Record<keyof Breakpoints, Function>
 
 // iterate through the sizes and create a media template
-export const media = Object.keys(theme.breakpoints).reduce(
+export const media: Partial<Media> = Object.keys(theme.breakpoints).reduce(
   (accumulator, label) => {
     // use rem in breakpoints to work properly cross-browser and support users
     // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
-    const emSize = theme.breakpoints[label] / 16
+    const remSize = theme.breakpoints[label] / 16
     accumulator[label] = (...args) =>
       css`
-        @media (min-width: ${emSize}rem) {
+        @media (min-width: ${remSize}rem) {
           ${css(...args)};
         }
       `
