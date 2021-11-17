dbe8ade9dbbb86020cd3a6061c17b296bae6cddc
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2021-11-18 05:35:21 +0800

    refactor header to use menu instead

--- components/Utils/Header/index.tsx
+++ components/Utils/Header/index.tsx
@@ -1,9 +1,8 @@
-import { Transition, Popover } from '@headlessui/react'
+import { Transition, Menu } from '@headlessui/react'
 import { Fragment, useContext } from 'react'
 import Link from 'next/link'
 import { UserContext } from '@helpers/user'
 import SignedInUser from './SignedInUser'
-import HeaderLink, { HeaderItem } from './HeaderLink'
 import DarkToggle from './DarkToggle'
 
 const links: Array<HeaderItem> = [
@@ -21,14 +20,14 @@ const Header = () => {
   const { user, setUser } = useContext(UserContext)
 
   return (
-    <Popover as='header' className='fixed inset-x-0 top-0 z-30 py-3 bg-primary'>
+    <Menu as='header' className='fixed inset-x-0 top-0 z-30 py-3 bg-primary'>
       {({ open }) => (
         <div className='container px-3 mx-auto'>
           <div className='flex items-center justify-between'>
             <div className='flex items-center text-secondary md:items-end'>
-              <Popover.Button className='p-1 mr-3 material-icons-sharp text-secondary md:hidden hover:opacity-75 focus:outline-none focus:ring-inset focus:ring-1 focus:ring-accent focus:ring-opacity-50 focus:ring-offset-primary'>
+              <Menu.Button className='p-1 mr-3 material-icons-sharp text-secondary md:hidden hover:opacity-75 focus:outline-none focus:ring-inset focus:ring-1 focus:ring-accent focus:ring-opacity-50 focus:ring-offset-primary'>
                 {open ? 'close' : 'menu'}
-              </Popover.Button>
+              </Menu.Button>
               <Link href='/'>
                 <a
                   id='Home'
@@ -39,8 +38,29 @@ const Header = () => {
                 </a>
               </Link>
               <nav className='hidden space-x-3 md:flex'>
-                {links.map(link => (
-                  <HeaderLink key={link.text} {...link} />
+                {links.map(({ isExternal, text, href }: HeaderItem)  => (
+                  <Fragment key={text}>
+                    {isExternal ? (
+                      <a
+                        href={href}
+                        target='_blank'
+                        rel='noreferrer noopener'
+                        className='align-text-bottom text-secondary hover:underline focus:outline-none focus:ring-1 focus:ring-accent focus:ring-opacity-50 focus:ring-offset-4 focus:ring-offset-primary'
+                        data-cy={`nav-${text}`}
+                      >
+                        {text}
+                      </a>
+                    ) : (
+                    <Link href={href} passHref>
+                      <a
+                        className='align-text-bottom text-secondary hover:underline focus:outline-none focus:ring-1 focus:ring-accent focus:ring-opacity-50 focus:ring-offset-4 focus:ring-offset-primary'
+                        data-cy={`nav-${text}`}
+                      >
+                        {text}
+                      </a>
+                    </Link>
+                    )}
+                  </Fragment>
                 ))}
               </nav>
             </div>
@@ -50,16 +70,11 @@ const Header = () => {
                 // user ? (
                 //   <SignedInUser setUser={setUser} name={user.name} id={user._id} />
                 // ) : null
-                // <Link href='/membership'>
-                //   <Button
-                //     outline
-                //     size='sm'
-                //     color='secondary'
-                //     className='d-none d-md-block rounded-0'
-                //   >
-                //     Membership
-                //   </Button>
-                // </Link>
+                <Link href='/membership'>
+                  <a className='px-4 py-1.5 my-px bg-transparent border border-secondary text-secondary hover:bg-secondary hover:text-primary focus:outline-none focus:ring-opacity-0 focus:bg-secondary focus:text-primary'>
+                    Membership
+                  </a>
+                </Link>
               }
             </div>
           </div>
@@ -72,19 +87,47 @@ const Header = () => {
             leaveFrom='opacity-100 translate-y-0'
             leaveTo='opacity-0 translate-y-1'
           >
-            <Popover.Panel
+            <Menu.Items
+              static
               as='nav'
               className='flex flex-col mt-3 space-y-3 ml-11 md:hidden max-w-min'
             >
-              {links.map(link => (
-                <HeaderLink key={link.text} {...link} />
+              {links.map(({ isExternal, text, href }: HeaderItem) => (
+                <Menu.Item key={text}>
+                  {({ active }) => isExternal ? (
+                    <a
+                      href={href}
+                      target='_blank'
+                      rel='noreferrer noopener'
+                      className={`align-text-bottom text-secondary ${active && 'underline'}`}
+                      data-cy={`nav-${text}`}
+                    >
+                      {text}
+                    </a>
+                  ) : (
+                    <Link href={href} passHref>
+                      <a
+                        className={`align-text-bottom text-secondary ${active && 'underline'}`}
+                        data-cy={`nav-${text}`}
+                      >
+                        {text}
+                      </a>
+                    </Link>
+                  )}
+                </Menu.Item>
               ))}
-            </Popover.Panel>
+            </Menu.Items>
           </Transition>
         </div>
       )}
-    </Popover>
+    </Menu>
   )
 }
 
 export default Header
+
+interface HeaderItem {
+  href: string
+  text: string
+  isExternal?: boolean
+}
