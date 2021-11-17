69a696a409ead37e27a154d6a20fe6d9c0ee766e
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2021-11-18 05:33:22 +0800

    reduce padding for mobile

--- components/Utils/Title/index.tsx
+++ components/Utils/Title/index.tsx
@@ -7,7 +7,7 @@ const Title = ({
   children: string
   typed?: boolean
 }) => (
-  <div className='flex items-center py-24 font-mono text-secondary bg-primary'>
+  <div className='flex items-center py-16 font-mono md:py-24 text-secondary bg-primary'>
     <div className='container px-3 mx-auto'>
       <h1 className='text-3xl'>
         {typed ? <TypedText text={[children]} /> : children}
