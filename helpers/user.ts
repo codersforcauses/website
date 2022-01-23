c22205d3b7aeff723a65e333a9859482d573d4af
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2022-01-23 17:42:04 +0800

    update user props and functions

--- helpers/user.ts
+++ helpers/user.ts
@@ -13,27 +13,27 @@ export const UserProvider = UserContext.Provider
 export const UserConsumer = UserContext.Consumer
 
 // All functions for user data should be placed below
-export const getInitials = (name: string) => {
-  const initialArray = name.split(' ').map(name => name.charAt(0))
-  return `${initialArray[0]}${initialArray[initialArray.length - 1]}`
+export const getInitials = (name: string | null) => {
+  const initialArray = name?.split(' ').map(name => name.charAt(0))
+  return initialArray
+    ? `${initialArray[0]}${initialArray[initialArray.length - 1]}`
+    : ''
 }
 
 export interface UserProps {
   _id: string
-  signUpType: string
   isGuildMember: boolean
   gender: string
   roles: Array<string>
   isFinancialMember: boolean
   firstName: string
   lastName: string
-  name?: string
+  name: string
   email: string
-  awsSub: string
   bio: string
   socials: Array<any>
   services: Array<any>
   createdAt: string
   updatedAt: string
-  jwtToken?: string
+  avatar?: string
 }
