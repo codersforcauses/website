24ed5f137683da41ac63db6937c5f2280ef77fdb
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2022-01-23 17:44:22 +0800

    create global variables

---
+++ lib/global.ts
@@ -0,0 +1,27 @@
+export const genderOptions = [
+  {
+    label: 'Male',
+    value: 'male'
+  },
+  {
+    label: 'Female',
+    value: 'female'
+  },
+  {
+    label: 'Non-binary',
+    value: 'other'
+  }
+]
+
+export const roles = [
+  'president',
+  'vice_president',
+  'secretary',
+  'treasurer',
+  'tech_lead',
+  'marketing_officer',
+  'OCM',
+  'first_year_rep',
+  'admin',
+  'HLM'
+]
