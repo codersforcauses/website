2061a09d6ef623a2691639eeb17b9e9783a9e145
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2022-01-26 15:50:06 +0800

    updated text styling

--- components/About/Committee/CommitteeCard/index.tsx
+++ components/About/Committee/CommitteeCard/index.tsx
@@ -25,7 +25,7 @@ const CommitteeCard = ({
     </div>
     <div className='absolute inset-x-0 bottom-0 p-4 transition-opacity duration-300 opacity-0 text-secondary group-hover:bg-primary group-hover:opacity-100'>
       <p className='font-mono font-black'>{name}</p>
-      <p className='mb-1 opacity-75'>{position}</p>
+      <p className='mb-1 text-primary/75'>{position}</p>
       <p className='text-sm'>{about}</p>
       <div className='flex items-center space-x-2'>
         {Object.keys(social).map(item => (
