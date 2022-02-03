59ef8d0005b28f8edeb6c75d31deebef150ba728
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2022-02-03 18:35:50 +0800

    fix width of dropdown

--- components/Utils/Header/SignedInUser/index.tsx
+++ components/Utils/Header/SignedInUser/index.tsx
@@ -70,7 +70,7 @@ const UserMenu = () => {
           >
             <Menu.Items
               static
-              className='w-[8.5rem] absolute right-0 flex flex-col items-stretch py-2 mt-2 space-y-1 border border-secondary bg-primary focus:outline-none'
+              className='min-w-[8.5rem] w-full absolute right-0 flex flex-col items-stretch py-2 mt-2 space-y-1 border border-secondary bg-primary focus:outline-none'
             >
               {Links.map(link => (
                 <Menu.Item key={link.icon}>
