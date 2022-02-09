5391b6f3cb807ab5b554d9510e456bb7c1c71d3f
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2022-02-10 01:57:52 +0800

    add square card input with styles but no submit

---
+++ components/Dashboard/DashboardPage/CardPayment/index.tsx
@@ -0,0 +1,155 @@
+import { memo, useEffect, useRef, useState } from 'react'
+import { useTheme } from 'next-themes'
+import { payments, Card, CardClassSelectors } from '@square/web-sdk'
+import { Button } from '@elements/Button'
+import BrandIcons from '@elements/BrandIcons'
+
+const applicationID = process.env.NEXT_PUBLIC_APPLICATION_ID || ''
+const locationID = process.env.NEXT_PUBLIC_LOCATION_ID || ''
+
+const commonStyles: CardClassSelectors = {
+  'input.is-error': {
+    color: '#FF0000'
+  },
+  'input::placeholder': {
+    color: '#6b7280'
+  },
+  '.input-container.is-error': {
+    borderColor: '#FF0000'
+  },
+  '.message-icon.is-error': {
+    color: '#FF0000'
+  },
+  '.message-text.is-error': {
+    color: '#FF0000'
+  }
+}
+const lightTheme: CardClassSelectors = {
+  input: {
+    backgroundColor: '#E9ECEF',
+    color: '#000000',
+    fontFamily: 'helvetica neue, sans-serif'
+  },
+  '.input-container': {
+    borderRadius: '0',
+    borderColor: '#000000'
+  },
+  '.input-container.is-focus': {
+    borderColor: '#000000'
+  },
+  '.message-icon': {
+    color: '#000000'
+  },
+  '.message-text': {
+    color: '#000000'
+  }
+}
+const darkTheme: CardClassSelectors = {
+  input: {
+    backgroundColor: '#000000',
+    color: '#FFFFFF',
+    fontFamily: 'helvetica neue, sans-serif'
+  },
+  '.input-container': {
+    borderRadius: '0',
+    borderColor: '#FFFFFF'
+  },
+  '.input-container.is-focus': {
+    borderColor: '#FFFFFF'
+  },
+  '.message-icon': {
+    color: '#FFFFFF'
+  },
+  '.message-text': {
+    color: '#FFFFFF'
+  }
+}
+
+const CardPayment = () => {
+  const [card, setCard] = useState<Card>()
+  const { resolvedTheme: theme } = useTheme()
+  const isDark = theme === 'dark'
+  const cardRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const loadCard = async () => {
+      try {
+        const paymentResponse = await payments(applicationID, locationID)
+        if (paymentResponse === null)
+          throw new Error('Square Web Payments SDK failed to load')
+        paymentResponse.setLocale('en-AU')
+        const cardResponse = await paymentResponse.card({
+          style: {
+            ...commonStyles,
+            ...(isDark ? darkTheme : lightTheme)
+          }
+        })
+        await cardResponse.attach('#card-container')
+        await cardResponse.focus('cardNumber')
+        setCard(cardResponse)
+
+        const cardMessage = cardRef.current?.children[0]
+          .children[1] as HTMLSpanElement
+        cardMessage.style.fontFamily = 'IBM Plex Sans'
+      } catch (error: any) {
+        console.log(error.message)
+      }
+    }
+    if (applicationID && locationID) {
+      if (card) {
+        card.configure({
+          style: {
+            ...commonStyles,
+            ...(isDark ? darkTheme : lightTheme)
+          }
+        })
+        const cardMessage = cardRef.current?.children[0]
+          .children[1] as HTMLSpanElement
+        cardMessage.style.fontFamily = 'IBM Plex Sans'
+
+        card.focus('cardNumber')
+      } else loadCard()
+    }
+  }, [card, isDark])
+
+  // const cardResponse = (token, buyer) => {
+  //   console.log(token?.details.card.brand)
+  // }
+
+  return (
+    <div className='relative'>
+      <div className='absolute right-0 z-10 top-[99px] md:top-[51px] flex items-center text-xs select-none justify-self-end opacity-50'>
+        powered by
+        <BrandIcons
+          icon='square'
+          dimensions={12}
+          className='mr-0.5 ml-1 fill-current'
+        />
+        square
+      </div>
+      <form
+        id='payment-form'
+        className='min-h-[191px] md:min-h-[142px] focus:outline-none'
+      >
+        <div
+          ref={cardRef}
+          id='card-container'
+          className='min-h-[145px] md:min-h-[96px] focus:outline-none'
+        >
+          {!card && (
+            <div className='w-full h-full'>
+              <div className='grid w-full h-[99px] md:h-12 font-mono border place-items-center animate-pulse border-primary dark:border-secondary bg-secondary dark:bg-alt-dark'>
+                Loading secure payment portal
+              </div>
+            </div>
+          )}
+        </div>
+        <Button fill className='w-full'>
+          Pay $5 membership
+        </Button>
+      </form>
+    </div>
+  )
+}
+
+export default memo(CardPayment)
