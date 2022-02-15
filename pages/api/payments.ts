e253bc5afa95d55dd00f5481610828ca50e5194d
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2022-02-15 19:37:07 +0800

    create payment route

--- pages/api/payments.ts
+++ pages/api/payments.ts
@@ -1,39 +1,77 @@
 import crypto from 'crypto'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { User } from '@helpers/global'
 
 const paymentHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const {
     body: { email, token, id }
   } = req
 
+  const paymentURL =
+    process.env.NODE_ENV === 'production'
+      ? 'https://connect.squareup.com/v2/payments'
+      : 'https://connect.squareupsandbox.com/v2/payments'
+
+  const url = process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : 'http://localhost:3000'
+
   if (req.method === 'POST') {
     try {
-      const response = await (
-        await fetch('https://connect.squareup.com/v2/payments', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: process.env.ACCESS_TOKEN!,
-            'Square-Version': '2022-01-20'
+      const userPromise = fetch(`${url}/api/users?id=${id}`)
+      const paymentPromise = fetch(paymentURL, {
+        method: 'POST',
+        headers: {
+          'Square-Version': '2022-01-20',
+          Authorization: `Bearer ${process.env.SQUARE_ACCESS_TOKEN}`,
+          'Content-Type': 'application/json',
+          mode: 'cors'
+        },
+        body: JSON.stringify({
+          amount_money: {
+            amount: 500,
+            currency: 'AUD'
           },
-          body: JSON.stringify({
-            amount_money: {
-              amount: 5.0,
-              currency: 'AUD'
-            },
-            idempotency_key: crypto.randomUUID(),
-            source_id: token || 'cnon:card-nonce-ok',
-            buyer_email_address: email
-          })
+          idempotency_key: crypto.randomUUID(),
+          source_id: token,
+          buyer_email_address: email
         })
-      ).json()
+      })
 
-      res.status(200).json({})
-    } catch ({ message }) {
+      const [userResponse, paymentResponse] = await Promise.all([
+        userPromise,
+        paymentPromise
+      ])
+      const user: User = await userResponse.json()
+      const { payment } = await paymentResponse.json()
+      const { card } = payment.card_details
+
+      await fetch(`${url}/api/users?id=${id}`, {
+        method: 'PATCH',
+        body: JSON.stringify({
+          cards: [
+            ...user?.cards!,
+            {
+              id: payment.id,
+              details: {
+                brand: card.card_brand,
+                last4: card.last_4,
+                expMonth: card.exp_month,
+                expYear: card.exp_year
+              }
+            }
+          ]
+        })
+      })
+
+      res.status(200).end(`Created card for ${user?.name}`)
+    } catch (err: any) {
+      console.log(err)
+
       res
         .status(400)
         .end(
-          message ||
+          err.message ||
             'An unexpected error occurred. Please refresh the page and try again.'
         )
     }
