ddb78546aad4c6a285986822f8af6a8b89a50696
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2022-03-14 01:36:12 +0800

    move files to array instead

--- components/Home/Services/index.tsx
+++ components/Home/Services/index.tsx
@@ -1,5 +1,31 @@
 import { memo } from 'react'
-import services from 'data/services.json'
+
+const services = [
+  {
+    icon: 'devices',
+    title: 'Applications',
+    description:
+      'Build custom web and mobile applications to engage with your audience'
+  },
+  {
+    icon: 'web',
+    title: 'Websites',
+    description:
+      'Build new websites or optimise existing pages to improve online visibility'
+  },
+  {
+    icon: 'storage',
+    title: 'Data Storage',
+    description:
+      'Design and create databases for efficient information storage and retrieval'
+  },
+  {
+    icon: 'how_to_reg',
+    title: 'Consulting',
+    description:
+      'Empower your organisation through technical knowledge and advice'
+  }
+]
 
 const Services = () => (
   <div className='grid grid-cols-1 grid-rows-4 gap-8 sm:grid-cols-2 sm:grid-rows-2 lg:grid-cols-4 lg:grid-rows-1'>
