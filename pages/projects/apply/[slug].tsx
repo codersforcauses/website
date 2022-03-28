23edbce96dc57b69c95d411a6b05ef5c924a32f6
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2022-03-28 08:14:05 +0800

    update ssr

--- pages/projects/apply/[slug].tsx
+++ pages/projects/apply/[slug].tsx
@@ -4,33 +4,27 @@ import { withServerSideAuth } from '@clerk/nextjs/ssr'
 import Meta from '@components/Utils/Meta'
 import ProjectApplicationPage from '@components/Projects/ProjectApplicationPage'
 
-const ProjectApplication = ({ id }: ProjectApplicationProps) => {
-  useEffect(() => {
-    !id && Router.replace('/membership')
-  }, [id])
+const ProjectApplication = () => (
+  <>
+    <Meta
+      title='Projects'
+      name='Our Projects'
+      page='projects'
+      description='A list of all past and present projects.'
+      image='https://og-social-cards.vercel.app/**.%2Fprojects**.png?theme=dark&md=1&fontSize=125px&images=https%3A%2Fcodersforcauses.org%2Flogo%2Fcfc_logo_white_full.svg'
+    />
+    <ProjectApplicationPage />
+  </>
+)
 
-  return (
-    <>
-      <Meta
-        title='Projects'
-        name='Our Projects'
-        page='projects'
-        description='A list of all past and present projects.'
-        image='https://og-social-cards.vercel.app/**.%2Fprojects**.png?theme=dark&md=1&fontSize=125px&images=https%3A%2Fcodersforcauses.org%2Flogo%2Fcfc_logo_white_full.svg'
-      />
-      {id && <ProjectApplicationPage />}
-    </>
-  )
-}
+export const getServerSideProps = withServerSideAuth(({ req: { auth } }) => {
+  const { sessionId } = auth
 
-export const getServerSideProps = withServerSideAuth(({ auth }) => ({
-  props: {
-    id: auth.userId || ''
+  return {
+    ...(!sessionId
+      ? { redirect: { destination: '/membership', permanent: false } }
+      : { props: {} })
   }
-}))
-
-interface ProjectApplicationProps {
-  id: string
-}
+})
 
 export default ProjectApplication
