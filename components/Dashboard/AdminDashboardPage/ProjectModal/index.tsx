191034a17c4f9e64aec504331a6e4951bf6bfd3d
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2022-04-03 10:17:42 +0800

    add project modal starter

--- components/Dashboard/AdminDashboardPage/ProjectModal/index.tsx
+++ components/Dashboard/AdminDashboardPage/ProjectModal/index.tsx
@@ -3,12 +3,26 @@ import { ProjectModel, ModalProps } from '@lib/types'
 import Modal from '@elements/Modal'
 import Alert from '@elements/Alert'
 import { Button } from '@elements/Button'
-import { Form, RadioGroup, TextArea, TextField } from '@elements/FormElements'
+import {
+  CheckField,
+  Form,
+  RadioGroup,
+  TextArea,
+  TextField,
+  TextFieldArray
+} from '@elements/FormElements'
 import validationSchema from './validation'
+
+const appType = ['Web', 'Mobile', 'Desktop']
 
 const defaultValues: Partial<ProjectModel> = {
   name: '',
-  description: ''
+  description: '',
+  dates: {
+    start: new Date(),
+    end: new Date()
+  },
+  impact: ['']
 }
 
 const ProjectModal = ({ closeModal, ...props }: ProjectModalProps) => {
@@ -17,7 +31,14 @@ const ProjectModal = ({ closeModal, ...props }: ProjectModalProps) => {
     async values => {
       try {
         setError('')
-        console.log(values)
+        console.log(values, values.type.filter(Boolean))
+
+        const slugify = values.name
+          .toLowerCase()
+          .trim()
+          .replace(/[^\w\s-]/g, '')
+          .replace(/[\s_-]+/g, '_')
+          .replace(/^-+|-+$/g, '')
 
         // await fetch('/api/projects', {
         //   method: 'POST',
@@ -55,6 +76,38 @@ const ProjectModal = ({ closeModal, ...props }: ProjectModalProps) => {
           label='Description'
           name='description'
           placeholder='A platform to group members to help at schools'
+          // rules={validationSchema.html}
+        />
+        <div className='grid grid-cols-3 gap-2'>
+          <p className='col-span-3 font-mono'>Application type</p>
+          {appType.map((app, idx) => (
+            <CheckField
+              key={app}
+              label={app}
+              name={`type.${idx}`}
+              value={app}
+            />
+          ))}
+        </div>
+        <div className='grid gap-2 sm:grid-cols-2'>
+          <TextField
+            type='date'
+            label='Start date'
+            name='dates.start'
+            pattern='\d{2}-\d{2}-\d{4}'
+            // rules={validationSchema.html}
+          />
+          <TextField
+            type='date'
+            label='End date'
+            name='dates.end'
+            pattern='\d{2}-\d{2}-\d{4}'
+            // rules={validationSchema.html}
+          />
+        </div>
+        <TextFieldArray
+          label='Potential impact'
+          name='impact'
           // rules={validationSchema.html}
         />
         <Button type='submit'>Submit</Button>
