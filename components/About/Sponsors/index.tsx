bcac33d4bea291c1d8569de3ad012bc1377111b4
Jack Sun <jsun1590@gmail.com>
2022-05-27 17:31:15 +0800

    U Group Variations

--- components/About/Sponsors/index.tsx
+++ components/About/Sponsors/index.tsx
@@ -1,10 +1,17 @@
 import { memo } from 'react'
 import ImageGrid from '@components/Utils/ImageGrid'
 import sponsors from 'data/sponsors.json'
+import { useTheme } from 'next-themes'
 
 const Sponsors = () => {
+  const { resolvedTheme: theme, setTheme } = useTheme()
   const imageList = sponsors.map(sponsor => ({
-    src: sponsor.logo,
+    src:
+      'dark' === theme
+        ? sponsor.logo
+        : sponsor.dark_logo === ''
+        ? sponsor.logo
+        : sponsor.dark_logo,
     alt: sponsor.name
   }))
 
