6468f0b4ce1ba383cb1232594071ba30e62a93e8
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2024-01-15 13:26:36 +0800

    add env variables

--- src/env.js
+++ src/env.js
@@ -18,6 +18,7 @@ export const env = createEnv({
       .enum(["development", "test", "production"])
       .default("development"),
     CLERK_SECRET_KEY: z.string(),
+    SQUARE_ACCESS_TOKEN: z.string(),
   },
 
   /**
@@ -27,6 +28,14 @@ export const env = createEnv({
    */
   client: {
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
+    NEXT_PUBLIC_SQUARE_APP_ID: z
+      .string()
+      .refine(
+        (str) =>
+          !(str.includes("sandbox") && process.env.NODE_ENV === "production"),
+        "Update Square keys to production keys",
+      ),
+    NEXT_PUBLIC_SQUARE_LOCATION_ID: z.string(),
   },
 
   /**
@@ -39,6 +48,9 @@ export const env = createEnv({
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    SQUARE_ACCESS_TOKEN: process.env.NEXT_PUBLIC_SQUARE_ACCESS_TOKEN,
+    NEXT_PUBLIC_SQUARE_APP_ID: process.env.NEXT_PUBLIC_SQUARE_APP_ID,
+    NEXT_PUBLIC_SQUARE_LOCATION_ID: process.env.NEXT_PUBLIC_SQUARE_LOCATION_ID,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
