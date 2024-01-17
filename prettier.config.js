c6d645d7abdf031b01af4670d2ab7deccfeadf63
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2024-01-18 02:39:04 +0800

    fix prettier typing and issues

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,13 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   trailingComma: "all",
   semi: false,
-  endofline: "lf",
-  maxLength: 120,
+  endOfLine: "lf",
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: true,
+  tailwindConfig: "./tailwind.config.ts",
   plugins: ["prettier-plugin-tailwindcss"],
-};
+}
 
-export default config;
+export default config
