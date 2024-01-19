a445a4cf3dcf4173ac4bb99f285736331a786a78
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2024-01-19 17:21:10 +0800

    update router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post"
 import { userRouter } from "~/server/api/routers/user"
 import { createTRPCRouter } from "~/server/api/trpc"
 
@@ -8,7 +7,6 @@ import { createTRPCRouter } from "~/server/api/trpc"
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   user: userRouter,
 })
 
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,32 +1,34 @@
-import { z } from "zod"
+// !remains as an example
 
-import { createTRPCRouter, protectedProcedure, publicProcedure } from "~/server/api/trpc"
-import { posts } from "~/server/db/schema"
+// import { z } from "zod"
 
-export const postRouter = createTRPCRouter({
-  hello: publicProcedure.input(z.object({ text: z.string() })).query(({ input }) => {
-    return {
-      greeting: `Hello ${input.text}`,
-    }
-  }),
+// import { createTRPCRouter, protectedProcedure, publicProcedure } from "~/server/api/trpc"
+// import { posts } from "~/server/db/schema"
 
-  create: protectedProcedure.input(z.object({ name: z.string().min(1) })).mutation(async ({ ctx, input }) => {
-    // simulate a slow db call
-    await new Promise((resolve) => setTimeout(resolve, 1000))
+// export const postRouter = createTRPCRouter({
+//   hello: publicProcedure.input(z.object({ text: z.string() })).query(({ input }) => {
+//     return {
+//       greeting: `Hello ${input.text}`,
+//     }
+//   }),
 
-    await ctx.db.insert(posts).values({
-      name: input.name,
-      createdById: ctx.auth?.userId,
-    })
-  }),
+//   create: protectedProcedure.input(z.object({ name: z.string().min(1) })).mutation(async ({ ctx, input }) => {
+//     // simulate a slow db call
+//     await new Promise((resolve) => setTimeout(resolve, 1000))
 
-  getLatest: publicProcedure.query(({ ctx }) => {
-    return ctx.db.query.posts.findFirst({
-      orderBy: (posts, { desc }) => [desc(posts.createdAt)],
-    })
-  }),
+//     await ctx.db.insert(posts).values({
+//       name: input.name,
+//       createdById: ctx.auth?.userId,
+//     })
+//   }),
 
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!"
-  }),
-})
+//   getLatest: publicProcedure.query(({ ctx }) => {
+//     return ctx.db.query.posts.findFirst({
+//       orderBy: (posts, { desc }) => [desc(posts.createdAt)],
+//     })
+//   }),
+
+//   getSecretMessage: protectedProcedure.query(() => {
+//     return "you can now see this secret message!"
+//   }),
+// })
