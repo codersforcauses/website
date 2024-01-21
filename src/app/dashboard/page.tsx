648af5cbddcc7b7e1caf77625728ff4679451636
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2024-01-22 02:41:51 +0800

    remove old route

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link"
 import Image from "next/image"
 
-import { getServerAuthSession } from "~/server/auth"
 import { api } from "~/trpc/server"
 
 export default async function Dashboard() {
