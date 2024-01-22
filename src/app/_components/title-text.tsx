d3ca2027a5c60990b5a1a829f379cfb239da9dab
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2024-01-22 15:07:30 +0800

    fix alignment

--- src/app/_components/title-text.tsx
+++ src/app/_components/title-text.tsx
@@ -8,7 +8,9 @@ interface TypedTitleTextProps {
 const TitleText = ({ children, typed = false }: TypedTitleTextProps) => {
   return (
     <div className="flex items-center bg-black py-16 font-mono text-white md:py-24">
-      <h1 className="container text-2xl md:text-3xl">{typed ? <TypedText text={[children]} /> : children}</h1>
+      <h1 className="container min-h-8 text-2xl md:min-h-9 md:text-3xl">
+        {typed ? <TypedText text={[children]} /> : children}
+      </h1>
     </div>
   )
 }
