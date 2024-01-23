bb9b1468d836cb92697cd88c11dc9ee76d7f4fe4
skyheat <6574274+skyheat@users.noreply.github.com>
2024-01-23 13:58:55 +0800

    webkit bs

--- src/components/payment/online.tsx
+++ src/components/payment/online.tsx
@@ -465,18 +465,17 @@ const OnlinePaymentForm = ({
       >
         {!googlePay && <Skeleton className="h-10 w-full" />}
       </div>
-      <div id="apple-pay-container" />
-      <div id="apple-pay-button" />
-      {/* <div
+      <div
         id="apple-pay-button"
         className={cn(
           "overflow-hidden focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2",
           applePay && "bg-primary ring-offset-background",
         )}
+        style={{ WebkitAppearance: "-apple-pay-button" }}
         // onClick={handleApplePayment}
       >
         {!applePay && <Skeleton className="h-10 w-full" />}
-      </div> */}
+      </div>
     </form>
   )
 }
