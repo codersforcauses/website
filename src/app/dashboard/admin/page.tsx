3125679587c9890810cf4e3685f039c38979c8f0
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2024-01-26 03:14:51 +0800

    admin view init

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -2,24 +2,39 @@ import Link from "next/link"
 import Image from "next/image"
 import Pie, { ProvidedProps, PieArcDatum } from "@visx/shape/lib/shapes/Pie"
 
-// import { getServerAuthSession } from "~/server/auth"
-import { api } from "~/trpc/server"
 import { Button } from "~/components/ui/button"
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "~/components/ui/resizable"
+
+import { api } from "~/trpc/server"
 
 export default async function AdminDashboard() {
-  // const hello = await api.post.hello.query({ text: "from tRPC" })
-  // const session = await getServerAuthSession()
+  const users = await api.user.getAllUsers.query()
+
+  console.log(users)
 
   return (
     <main className="main">
-      <div className="container py-8">
-        <h1 className="text-2xl font-bold">Admin Dashboard</h1>
-        <div>
+      <ResizablePanelGroup direction="horizontal" className="h-full w-full">
+        <ResizablePanel defaultSize={25} className="min-w-12">
+          <div className="flex h-full items-center justify-center p-6">
+            <span className="font-semibold">Sidebar</span>
+          </div>
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel defaultSize={75}>
+          <div className="flex h-full items-center justify-center p-6">
+            <span className="font-semibold">Content</span>
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+      {/* <div className="container py-8">
+        <h1 className="text-2xl font-bold">Admin Dashboard</h1> */}
+      {/* <div>
           <Button asChild>
             <Link href="/projects/new">New project</Link>
           </Button>
-        </div>
-      </div>
+        </div> */}
+      {/* </div> */}
     </main>
   )
 }
