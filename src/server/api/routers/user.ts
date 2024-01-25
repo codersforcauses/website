e63d47206d6c9fd8c6fd409a73874d55115c5ad9
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2024-01-26 02:34:23 +0800

    add user functions

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from "@trpc/server"
 import { eq, or, sql } from "drizzle-orm"
 import { z } from "zod"
 
@@ -51,18 +52,115 @@ export const userRouter = createTRPCRouter({
       return user
     }),
 
+  login: publicProcedure.mutation(async ({ ctx }) => {
+    const id = ctx.user?.id
+    if (!id) return
+
+    const [user] = await ctx.db.select().from(users).where(eq(users.id, id))
+
+    return user
+  }),
+
   getUser: protectedProcedure.input(z.string()).query(async ({ ctx, input }) => {
     return await ctx.db.select().from(users).where(eq(users.id, input))
+  }),
+
+  getAllUsers: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.db.select().from(users)
   }),
 
   updateRole: protectedProcedure
     .input(
       z.object({
-        role: z.enum(["member", "committee", "executive", "past", "honorary", "admin"]).optional(),
+        id: z.string(),
+        role: z.enum(["member", "committee", "past", "honorary", "admin"]).nullable(),
       }),
     )
     .mutation(async ({ ctx, input }) => {
-      const user = await ctx.db.update(users).set({ role: input.role }).where(eq(users.id, ctx.user?.id))
+      if (!ctx.user.id)
+        throw new TRPCError({ code: "UNAUTHORIZED", message: "You must be logged in to update your details" })
+
+      const [currentUser] = await ctx.db.select().from(users).where(eq(users.id, ctx.user.id))
+      if (!currentUser)
+        throw new TRPCError({ code: "NOT_FOUND", message: "You must be logged in to update your details" })
+
+      // TODO visit logic
+      let user
+      if (currentUser.id === input.id) {
+        if (currentUser.role === "admin" || currentUser.role === "committee") {
+          user = await ctx.db.update(users).set({ role: input.role }).where(eq(users.id, input.id))
+        } else {
+          // cant change some else's role
+        }
+      } else {
+        if (input.role !== "member" || input.role !== null) {
+          throw new TRPCError({ code: "FORBIDDEN", message: "You cannot change your own role" })
+        } else {
+          user = await ctx.db.update(users).set({ role: input.role }).where(eq(users.id, ctx.user?.id))
+        }
+      }
+      // const user = await ctx.db.update(users).set({ role: input.role }).where(eq(users.id, ctx.user?.id))
+      console.log(user)
+
+      return user
+    }),
+
+  update: publicProcedure
+    .input(
+      z.object({
+        name: z
+          .string()
+          .min(2, {
+            message: "Name is required",
+          })
+          .optional(),
+        preferred_name: z
+          .string()
+          .min(2, {
+            message: "Preferred name is required",
+          })
+          .optional(),
+        email: z
+          .string()
+          .email({
+            message: "Invalid email address",
+          })
+          .min(2, {
+            message: "Email is required",
+          })
+          .optional(),
+        pronouns: z
+          .string()
+          .min(2, {
+            message: "Pronouns are required",
+          })
+          .optional(),
+        student_number: z.string().optional(),
+        uni: z.string().optional(),
+        github: z.string().optional(),
+        discord: z.string().optional(),
+        subscribe: z.boolean(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const currentUser = ctx.user
+
+      if (!currentUser)
+        throw new TRPCError({ code: "UNAUTHORIZED", message: "You must be logged in to update your details" })
+
+      const [dbUser] = await ctx.db.select().from(users).where(eq(users.id, currentUser.id))
+
+      const user = await ctx.db.update(users).set({
+        name: input.name ?? dbUser?.name,
+        preferred_name: input.preferred_name ?? dbUser?.preferred_name,
+        email: input.email ?? dbUser?.email,
+        pronouns: input.pronouns ?? dbUser?.pronouns,
+        student_number: input.student_number ?? dbUser?.student_number,
+        university: input.uni ?? dbUser?.university,
+        github: input.github ?? dbUser?.github,
+        discord: input.discord ?? dbUser?.discord,
+        subscribe: input.subscribe ?? dbUser?.subscribe,
+      })
       console.log(user)
 
       return user
