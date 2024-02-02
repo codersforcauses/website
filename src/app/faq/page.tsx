45e4c055c3f1d3cbedbc31cabc8aa0a09cf5e722
skyheat <6574274+skyheat@users.noreply.github.com>
2024-02-03 02:28:19 +0800

    added accordion, faq init

---
+++ src/app/faq/page.tsx
@@ -0,0 +1,87 @@
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "~/components/ui/accordion"
+import TitleText from "../_components/title-text"
+
+const FAQList = [
+  {
+    description: "Who can sign up to be a CFC member?",
+    answer:
+      "Our membership is not limited to UWA students. Everyone with an interest in software development is welcome to join.",
+  },
+  {
+    description: "How much does it cost to become a member?",
+    answer:
+      "Contact our friendly committee. You can do so on our Discord server. The fee for the year is $5. This is needed to participate in our projects and provides discounts for our paid events.",
+  },
+  {
+    description: "What do you offer during semester?",
+    answer: "We run technical workshops, social events and industry nights during semester.",
+  },
+  {
+    description: "Do projects run during semester as well?",
+    answer:
+      "Generally NO. But our projects are open source, so feel free to contribute whenever. If you need help getting started on a project, please reach out to our committee, we are happy to help.",
+  },
+  {
+    description: "What stack do you normally use for your projects?",
+    answer:
+      "In the past, we've worked with Javascript, Typescript and Node.js. If a framework is used, it will likely be either Vue/Nuxt.js or React/Next.js and an Express backend. In future projects, the software used can change depending on requirements.",
+  },
+  {
+    description: "Can I get credit from the university for the volunteer hours I contributed towards the project?",
+    answer:
+      "You certainly can, but only if you are a UWA student. During the project we will show you how to submit your hours to us for accreditation via Guild Volunteering.",
+  },
+  {
+    description: "Should I apply if I don't have any experience with HTML/CSS/JS?",
+    answer:
+      "It's strongly recommended but not necessary. The reason for that is we work with technologies that build on top of those basic fundamentals. Without knowing the basics, it might be hard to grasp more difficult concepts. If you would like some guidance on where to start, head to the #coding-resources section on our Discord server!",
+  },
+  {
+    description: "How many hours do I have to contribute per week towards the project?",
+    answer:
+      "This is completely up to you. We understand that people might be busy sometimes during the holidays and this is volunteer work, so we won't force people to commit to certain hours. However we will ask your availability during our applicant interview to get a rough idea, which helps us to plan and schedule our project resources and timeline.",
+  },
+  {
+    description: "What exactly will be expected from me to participate in the projects?",
+    answer:
+      "The project group will meet at campus 2 times per week, depending on each group's availability. During this time, you can get help from your mentors, learn about technical details or work on the project with other members. We will also invite industry partners to give short talks/workshops to get you familiarised with industry software practice.",
+  },
+]
+
+const FaqPage = () => {
+  return (
+    <main className="main">
+      <TitleText typed>./frequently asked questions</TitleText>
+      <div className="container flex flex-row space-x-12 py-12">
+        <div className="w-2/3">
+          {FAQList.map((faq, idx) => (
+            <Accordion type="multiple" key={idx} className="w-full">
+              <AccordionItem value={`item-${idx}`}>
+                <AccordionTrigger className="transition-colors duration-300 hover:bg-slate-100">
+                  {faq.description}
+                </AccordionTrigger>
+                <AccordionContent>{faq.answer}</AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          ))}
+        </div>
+        <div>
+          <h1>Quick Links</h1>
+          <div className="flex flex-col space-y-2">
+            <div className="h-12 bg-slate-300">
+              <h3>Join our Discord Community</h3>
+            </div>
+            <div className="h-12 bg-slate-300">
+              <h3>Join our Discord Community</h3>
+            </div>
+            <div className="h-12 bg-slate-300">
+              <h3>Join our Discord Community</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default FaqPage
