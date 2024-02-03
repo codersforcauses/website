3915c67f6bbf238ef5d7165c526902413e9438f7
skyheat <6574274+skyheat@users.noreply.github.com>
2024-02-03 11:23:35 +0800

    updated app_id

--- src/components/payment/online.tsx
+++ src/components/payment/online.tsx
@@ -34,7 +34,7 @@ interface OnlinePaymentFormProps {
 }
 
 const APP_ID = env.NEXT_PUBLIC_SQUARE_APP_ID
-console.log(APP_ID)
+console.log("app_id", APP_ID)
 const LOCATION_ID = env.NEXT_PUBLIC_SQUARE_LOCATION_ID
 const containerID = "card-container"
 const googlePayID = "google-pay-container"
