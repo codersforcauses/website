1ab438f853a3b40187b44d5571d82c6fa335bd12
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2024-02-03 16:44:49 +0800

    admin users table

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,40 +1,81 @@
+"use client"
+
+import * as React from "react"
 import Link from "next/link"
 import Image from "next/image"
 import Pie, { ProvidedProps, PieArcDatum } from "@visx/shape/lib/shapes/Pie"
 
-import { Button } from "~/components/ui/button"
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "~/components/ui/resizable"
+import { ToggleGroup, ToggleGroupItem } from "~/components/ui/toggle-group"
+import { cn } from "~/lib/utils"
+import { type ImperativePanelHandle } from "react-resizable-panels"
+import { Separator } from "~/components/ui/separator"
+import UserData from "./_components/user-table"
 
-import { api } from "~/trpc/server"
+const sidebarItems = [
+  { text: "Users", slug: "users", icon: "group" },
+  { text: "Projects", slug: "projects", icon: "devices" },
+  { text: "Events", slug: "events", icon: "event" },
+  { text: "Analytics", slug: "analytics", icon: "analytics" },
+]
 
-export default async function AdminDashboard() {
-  const users = await api.user.getAll.query()
-
-  console.log(users)
+export default function AdminDashboard() {
+  const sidebarRef = React.useRef<ImperativePanelHandle>(null)
+  const [collapsed, setCollapsed] = React.useState(false)
 
   return (
     <main className="main">
-      <ResizablePanelGroup direction="horizontal" className="h-full w-full">
-        <ResizablePanel defaultSize={25} className="min-w-12">
-          <div className="flex h-full items-center justify-center p-6">
-            <span className="font-semibold">Sidebar</span>
-          </div>
-        </ResizablePanel>
-        <ResizableHandle />
-        <ResizablePanel defaultSize={75}>
-          <div className="flex h-full items-center justify-center p-6">
-            <span className="font-semibold">Content</span>
-          </div>
-        </ResizablePanel>
-      </ResizablePanelGroup>
-      {/* <div className="container py-8">
-        <h1 className="text-2xl font-bold">Admin Dashboard</h1> */}
-      {/* <div>
-          <Button asChild>
-            <Link href="/projects/new">New project</Link>
-          </Button>
-        </div> */}
-      {/* </div> */}
+      <div className="container h-full py-4">
+        <ResizablePanelGroup direction="horizontal">
+          <ResizablePanel
+            ref={sidebarRef}
+            tagName="nav"
+            collapsible
+            defaultSize={17}
+            maxSize={25}
+            minSize={17}
+            collapsedSize={4}
+            className={cn(
+              collapsed ? "w-[50px] transition-all duration-300 ease-in-out" : "max-w-[250px]",
+              "box-border min-w-[50px] bg-muted",
+            )}
+            onCollapse={() => setCollapsed(true)}
+            onExpand={() => setCollapsed(false)}
+          >
+            <div className="flex items-center p-1">
+              <div
+                className={cn(
+                  "grid size-[42px] min-w-[42px] place-items-center bg-black font-mono font-semibold text-white dark:bg-white dark:text-black",
+                  collapsed && "aspect-square w-full",
+                )}
+              >
+                cfc
+              </div>
+              {!collapsed && <h1 className="ml-2 font-mono font-bold leading-tight">Admin Dashboard</h1>}
+            </div>
+            <Separator className="bg-background" />
+            <ToggleGroup type="single" defaultValue={sidebarItems[0]?.slug} className="flex flex-col p-1">
+              {sidebarItems.map(({ slug, text, icon }) => (
+                <ToggleGroupItem
+                  key={slug}
+                  value={slug}
+                  className={cn(
+                    "box-border w-full data-[state=on]:bg-background data-[state=on]:text-foreground",
+                    !collapsed && "justify-start",
+                  )}
+                >
+                  <span className={cn("material-symbols-sharp size-6", !collapsed && "mr-2")}>{icon}</span>
+                  {!collapsed && text}
+                </ToggleGroupItem>
+              ))}
+            </ToggleGroup>
+          </ResizablePanel>
+          <ResizableHandle withHandle className="w-3 bg-background" />
+          <ResizablePanel defaultSize={75}>
+            <UserData />
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </div>
     </main>
   )
 }
