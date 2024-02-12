413b6eb0a37ee34a4763f6cf1f443a93a3029d34
skyheat <6574274+skyheat@users.noreply.github.com>
2024-02-13 01:19:52 +0800

    updated events to use Tabs

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -6,6 +6,7 @@ import EventCard from "./eventcard"
 import TitleText from "../_components/title-text"
 import day from "dayjs"
 import customParseFormat from "dayjs/plugin/customParseFormat"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs"
 
 import { type Event } from "~/lib/types"
 
@@ -30,43 +31,56 @@ const events = {
 type EventFilter = keyof typeof events
 
 const EventsPage = () => {
-  const [eventFilter, setEventFilter] = useState<EventFilter>("upcoming")
-
   return (
     <main className="main">
       <TitleText typed>./events</TitleText>
       <div className="bg-secondary text-primary dark:bg-alt-dark dark:text-secondary">
         <div className="container mx-auto space-y-12 px-3 py-12 md:py-24">
           <div className="mb-8 text-left">
-            <button
-              className={`px-4 py-2 ${eventFilter === "upcoming" ? "bg-primary text-white dark:bg-secondary" : "bg-white"}`}
-              onClick={() => setEventFilter("past")}
-            >
-              Past
-            </button>
-            <button
-              className={`${eventFilter === "past" ? "bg-primary text-white dark:bg-secondary" : "bg-white"} px-4 py-2`}
-              onClick={() => setEventFilter("upcoming")}
-            >
-              Upcoming
-            </button>
-          </div>
-          <div className="space-y-6">
-            {events[eventFilter].length === 0 ? (
-              <h2 className="font-mono text-4xl text-black dark:text-white">No {eventFilter} events</h2>
-            ) : (
-              events[eventFilter].map((event) => (
-                <div
-                  key={event.date + event.time.start}
-                  className="relative border-l border-primary border-opacity-40 dark:border-secondary dark:border-opacity-40"
-                >
-                  <span className="absolute left-0 -translate-x-1/2 translate-y-full rotate-90 transform bg-secondary px-4 font-mono text-lg dark:bg-alt-dark lg:translate-y-0 lg:rotate-0">
-                    {event.date}
-                  </span>
-                  <EventCard key={event.slug} {...event} />
+            <Tabs defaultValue="upcoming">
+              <TabsList className="mb-2">
+                <TabsTrigger value="past">Past</TabsTrigger>
+                <TabsTrigger value="upcoming">Upcoming</TabsTrigger>
+              </TabsList>
+              <TabsContent value="past">
+                <div className="space-y-6">
+                  {events.past.length === 0 ? (
+                    <h2 className="font-mono text-4xl text-black dark:text-white">No {eventFilter} events</h2>
+                  ) : (
+                    events.past.map((event) => (
+                      <div
+                        key={event.date + event.time.start}
+                        className="relative border-l border-primary border-opacity-40 dark:border-secondary dark:border-opacity-40"
+                      >
+                        <span className="absolute left-0 -translate-x-1/2 translate-y-full rotate-90 transform bg-secondary px-4 font-mono text-lg dark:bg-alt-dark lg:translate-y-0 lg:rotate-0">
+                          {event.date}
+                        </span>
+                        <EventCard key={event.slug} {...event} />
+                      </div>
+                    ))
+                  )}
                 </div>
-              ))
-            )}
+              </TabsContent>
+              <TabsContent value="upcoming">
+                <div className="space-y-6">
+                  {events.upcoming.length === 0 ? (
+                    <h2 className="font-mono text-4xl text-black dark:text-white">No {eventFilter} events</h2>
+                  ) : (
+                    events.upcoming.map((event) => (
+                      <div
+                        key={event.date + event.time.start}
+                        className="relative border-l border-primary border-opacity-40 dark:border-secondary dark:border-opacity-40"
+                      >
+                        <span className="absolute left-0 -translate-x-1/2 translate-y-full rotate-90 transform bg-secondary px-4 font-mono text-lg dark:bg-alt-dark lg:translate-y-0 lg:rotate-0">
+                          {event.date}
+                        </span>
+                        <EventCard key={event.slug} {...event} />
+                      </div>
+                    ))
+                  )}
+                </div>
+              </TabsContent>
+            </Tabs>
           </div>
         </div>
       </div>
