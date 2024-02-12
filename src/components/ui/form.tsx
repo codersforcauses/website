795f6892898a1b867dac28e96dc422324ae8bb8a
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2024-02-13 00:24:42 +0800

    remove random comment from Dylan

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -13,7 +13,7 @@ import {
 import { cn } from "~/lib/utils"
 import { Label } from "~/components/ui/label"
 
-const Form = FormProvider // ?_?
+const Form = FormProvider
 
 type FormFieldContextValue<
   TFieldValues extends FieldValues = FieldValues,
