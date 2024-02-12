17456a3459049448125d4db0466b0bc382d777be
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2024-02-13 01:12:51 +0800

    fixed middleware for admin routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,27 @@
 import { authMiddleware } from "@clerk/nextjs"
 import { NextResponse } from "next/server"
+import { getUserCookie } from "./app/actions"
 
-const protectedPages = ["/dashboard", "/dashboard/admin", "/profile/settings"]
+const adminRoles = ["admin", "committee"]
 
+const adminPages = ["/dashboard/admin"]
+const protectedPages = ["/dashboard", "/profile/settings"]
 const publicPages = ["/"]
 
 export default authMiddleware({
   publicRoutes: publicPages,
-  afterAuth(auth, req) {
+  async afterAuth(auth, req) {
     if (!auth.userId && protectedPages.includes(req.nextUrl.pathname)) {
       const joinURL = new URL("/join", req.nextUrl.origin)
       return NextResponse.redirect(joinURL)
     }
-
+    if (adminPages.includes(req.nextUrl.pathname)) {
+      const user = await getUserCookie()
+      if (!adminRoles.includes(user?.role ?? "")) {
+        const dashboardURL = new URL("/dashboard", req.nextUrl.origin)
+        return NextResponse.redirect(dashboardURL)
+      }
+    }
     return NextResponse.next()
   },
 })
