4f07da70197b20cf96304831e4cb2e3c55d4e488
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2024-02-16 03:42:14 +0800

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { type Metadata } from "next"
 import dynamic from "next/dynamic"
 import { IBM_Plex_Mono, IBM_Plex_Sans } from "next/font/google"
 import { ClerkProvider } from "@clerk/nextjs"
+import { Analytics } from "@vercel/analytics/react"
 
 import { type PropsWithChildren } from "~/lib/types"
 import { Toaster } from "~/components/ui/toaster"
@@ -49,6 +50,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
             <Toaster />
           </Providers>
           {process.env.NODE_ENV === "production" && <Messenger />}
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
