29d8b372587ed655876fc289dfe50a01769311c1
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2024-02-17 05:01:01 +0800

    remove useless deps

--- src/lib/bigint.d.ts
+++
@@ -1,6 +0,0 @@
-// This was done so that BigInt could be serialized by Response.json()
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt#use_within_json
-
-declare interface BigInt {
-  toJSON(): string
-}
