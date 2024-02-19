9f32982d5b5dd6a3e1804e003dcbac33fd73147a
Nicholas Choong <nicholascwc1997@gmail.com>
2024-02-19 18:47:39 +0800

    add gemini

---
+++ src/components/gemini/index.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { GoogleGeminiEffect } from "../ui/gemini"
+
+const Gemini = () => {
+  return (
+    <div className="flex h-full items-center bg-black">
+      <GoogleGeminiEffect className="display-block py-4" />
+    </div>
+  )
+}
+
+export default Gemini
