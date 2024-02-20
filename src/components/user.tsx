97c5531d31ed6473064142a98de54e41814d12b1
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2024-02-20 17:14:41 +0800

    update to only fetch when logged in

--- src/components/user.tsx
+++ src/components/user.tsx
@@ -1,11 +1,15 @@
 "use client"
 
+import { useAuth } from "@clerk/nextjs"
+
 import { setUserCookie } from "~/app/actions"
 import { api } from "~/trpc/react"
 
 // need to implement a better way
 const User = () => {
+  const { userId } = useAuth()
   api.user.getCurrent.useQuery(undefined, {
+    enabled: !!userId,
     refetchInterval: 1000 * 60 * 10, // 10 minutes
     onSuccess: (data) => {
       if (data) void setUserCookie(data)
