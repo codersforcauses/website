4466b10b83e097ea01f0258446b08a94220b37bb
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2024-02-21 03:34:21 +0800

    move middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs"
 import { NextResponse } from "next/server"
-import { getUserCookie } from "./app/actions"
+import { getUserCookie } from "./src/app/actions"
 
 const adminRoles = ["admin", "committee"]
 
