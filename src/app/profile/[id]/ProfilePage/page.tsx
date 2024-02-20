1230320be891301e4517d78263d32b1618d1b0ad
skyheat <6574274+skyheat@users.noreply.github.com>
2024-02-20 14:18:13 +0800

    update profile page

--- src/app/profile/[id]/ProfilePage/page.tsx
+++ src/app/profile/[id]/ProfilePage/page.tsx
@@ -5,7 +5,7 @@ import { siDiscord, siGithub } from "simple-icons"
 import { Badge } from "~/components/ui/badge"
 import { Button } from "~/components/ui/button"
 import { type User } from "~/lib/types"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import EditProfile from "./EditProfile/page"
 import { api } from "~/trpc/react"
 
