d1c14e0e83694014a78a010aa410da745c7c2f06
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2024-02-22 15:20:02 +0800

    fix build error

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,17 +5,18 @@ import { ThemeProvider } from "~/components/theme-provider"
 import { TRPCReactProvider } from "~/trpc/react"
 
 export default async function Providers({ children }: PropsWithChildren) {
-  return process.env.SKIP_ENV_VALIDATION ? (
+  return (
     <ThemeProvider enableSystem disableTransitionOnChange attribute="class" defaultTheme="system">
       <TRPCReactProvider cookies={cookies().toString()}>{children}</TRPCReactProvider>
     </ThemeProvider>
-  ) : (
-    import("@builder.io/react-hydration-overlay").then(({ HydrationOverlay }) => (
-      <HydrationOverlay>
-        <ThemeProvider enableSystem disableTransitionOnChange attribute="class" defaultTheme="system">
-          <TRPCReactProvider cookies={cookies().toString()}>{children}</TRPCReactProvider>
-        </ThemeProvider>
-      </HydrationOverlay>
-    ))
   )
+  // ) : (
+  //   import("@builder.io/react-hydration-overlay").then(({ HydrationOverlay }) => (
+  //     <HydrationOverlay>
+  //       <ThemeProvider enableSystem disableTransitionOnChange attribute="class" defaultTheme="system">
+  //         <TRPCReactProvider cookies={cookies().toString()}>{children}</TRPCReactProvider>
+  //       </ThemeProvider>
+  //     </HydrationOverlay>
+  //   ))
+  // )
 }
