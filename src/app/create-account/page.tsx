71e9eb0ce8a0c79b377756963d9ed572f0795a23
skyheat <6574274+skyheat@users.noreply.github.com>
2024-02-25 19:00:39 +0800

    added please do not close this page

--- src/app/create-account/page.tsx
+++ src/app/create-account/page.tsx
@@ -302,7 +302,7 @@ export default function CreateAccount() {
       toast({
         title: "Email Verification Sent!",
         description:
-          "We've sent you an email with a link to verify your email address. It can sometimes take up to 10 minutes to arrive.",
+          "We've sent you an email with a link to verify your email address. It can sometimes take up to 10 minutes to arrive. Please do not close this page.",
       })
 
       const su = await startEmailLinkFlow({
