e5fb405ef4f0d67e67e3846cd4cdfe78fa16ff82
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2024-03-04 13:29:25 +0800

    fix accidental dev stuff

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -6,17 +6,17 @@ import { TRPCReactProvider } from "~/trpc/react"
 
 export default async function Providers({ children }: PropsWithChildren) {
   return (
-    //   <ThemeProvider enableSystem disableTransitionOnChange attribute="class" defaultTheme="system">
-    //     <TRPCReactProvider cookies={cookies().toString()}>{children}</TRPCReactProvider>
-    //   </ThemeProvider>
-    // )
-    // ) : (
-    import("@builder.io/react-hydration-overlay").then(({ HydrationOverlay }) => (
-      <HydrationOverlay>
-        <ThemeProvider enableSystem disableTransitionOnChange attribute="class" defaultTheme="system">
-          <TRPCReactProvider cookies={cookies().toString()}>{children}</TRPCReactProvider>
-        </ThemeProvider>
-      </HydrationOverlay>
-    ))
+    <ThemeProvider enableSystem disableTransitionOnChange attribute="class" defaultTheme="system">
+      <TRPCReactProvider cookies={cookies().toString()}>{children}</TRPCReactProvider>
+    </ThemeProvider>
   )
+  // ) : (
+  // import("@builder.io/react-hydration-overlay").then(({ HydrationOverlay }) => (
+  //   <HydrationOverlay>
+  //     <ThemeProvider enableSystem disableTransitionOnChange attribute="class" defaultTheme="system">
+  //       <TRPCReactProvider cookies={cookies().toString()}>{children}</TRPCReactProvider>
+  //     </ThemeProvider>
+  //   </HydrationOverlay>
+  // ))
+  // )
 }
