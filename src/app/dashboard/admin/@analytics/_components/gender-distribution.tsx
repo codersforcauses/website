98cd33751289047ebb1b4762d6fd17b1f019bbd6
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2024-03-05 18:43:26 +0800

    add tooltip for gender

--- src/app/dashboard/admin/@analytics/_components/gender-distribution.tsx
+++ src/app/dashboard/admin/@analytics/_components/gender-distribution.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { Group, Responsive, Scale, Shape } from "@visx/visx"
+import { Group, Responsive, Shape, Tooltip } from "@visx/visx"
 
 interface DataProps {
   count: number
@@ -19,62 +19,58 @@ interface GraphProps extends GenderDistributionProps {
 
 const count = (d: DataProps) => d.count
 
+const getColor = (pronoun: string) => {
+  switch (pronoun) {
+    case "he/him":
+      return "#3b82f6"
+    case "she/her":
+      return "#ec4899"
+    case "they/them":
+      return "#10b981"
+    default:
+      return "#f59e0b"
+  }
+}
+
 const Graph = ({ width, height, data }: GraphProps) => {
+  const tooltip = Tooltip.useTooltip<DataProps>()
   const radius = Math.min(width, height) / 2
   const centerY = height / 2
   const centerX = width / 2
-  const donutThickness = 80
-
-  const Gender = data.map((d) => d.pronouns)
-
-  const getGenderColor = React.useMemo(
-    () =>
-      Scale.scaleOrdinal({
-        domain: Gender,
-        range: [
-          "#3b82f6",
-          "#ec4899",
-          "rgba(255,255,255,0.5)",
-          "rgba(255,255,255,0.4)",
-          "rgba(255,255,255,0.3)",
-          "rgba(255,255,255,0.2)",
-          "rgba(255,255,255,0.1)",
-        ],
-      }),
-    [],
-  )
 
   return (
-    <svg width={width} height={height}>
-      <Group.Group top={centerY} left={centerX}>
-        <Shape.Pie
-          data={data}
-          pieValue={count}
-          outerRadius={radius}
-          innerRadius={radius - donutThickness}
-          padAngle={0.005}
-        >
-          {({ arcs, path }) => (
-            <>
-              {arcs.map((arc) => {
+    <div>
+      <svg width={width} height={height}>
+        <Group.Group top={centerY} left={centerX}>
+          <Shape.Pie data={data} pieValue={count} outerRadius={radius} innerRadius={radius / 2} padAngle={0.005}>
+            {({ arcs, path }) =>
+              arcs.map((arc) => {
                 const [centroidX, centroidY] = path.centroid(arc)
                 const hasSpaceForLabel = arc.endAngle - arc.startAngle >= 0.1
 
                 return (
-                  <g key={arc.index}>
-                    <path
-                      // compute interpolated path d attribute from intermediate angle values
-                      d={path(arc) ?? ""}
-                      fill={getGenderColor(arc.data.pronouns)}
-                    />
+                  <g
+                    key={arc.index}
+                    onMouseLeave={() => {
+                      tooltip.hideTooltip()
+                    }}
+                    onMouseMove={(event) => {
+                      tooltip.showTooltip({
+                        tooltipLeft: event.clientX,
+                        tooltipTop: event.clientY,
+                        tooltipData: arc.data,
+                      })
+                    }}
+                  >
+                    <path d={path(arc) ?? ""} fill={getColor(arc.data.pronouns)} />
                     {hasSpaceForLabel && (
                       <g>
                         <text
                           fill="white"
                           x={centroidX}
                           y={centroidY}
-                          dy=".33em"
-                          fontSize={9}
+                          dy=".33rem"
+                          fontSize={12}
                           textAnchor="middle"
                           pointerEvents="none"
                         >
@@ -84,12 +80,23 @@ const Graph = ({ width, height, data }: GraphProps) => {
                     )}
                   </g>
                 )
-              })}
-            </>
-          )}
-        </Shape.Pie>
-      </Group.Group>
-    </svg>
+              })
+            }
+          </Shape.Pie>
+        </Group.Group>
+      </svg>
+      {tooltip.tooltipOpen && tooltip.tooltipData && (
+        <Tooltip.Tooltip
+          unstyled
+          applyPositionStyle
+          top={tooltip.tooltipTop}
+          left={tooltip.tooltipLeft}
+          className="text-bg-muted-foreground bg-muted p-2 text-sm"
+        >
+          {tooltip.tooltipData.pronouns}: {tooltip.tooltipData.count}
+        </Tooltip.Tooltip>
+      )}
+    </div>
   )
 }
 
