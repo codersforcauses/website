36906c93a0253616a650c24dfea313becc24436c
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2024-03-12 03:41:13 +0800

    update event page

---
+++ src/app/events/layout.tsx
@@ -0,0 +1,13 @@
+import type { PropsWithChildren } from "~/lib/types"
+import TitleText from "../_components/title-text"
+
+const Layout = ({ children }: PropsWithChildren) => {
+  return (
+    <main className="main">
+      <TitleText typed>./events</TitleText>
+      {children}
+    </main>
+  )
+}
+
+export default Layout
--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,95 +1,123 @@
-"use client"
+import { format, isAfter, isBefore, isSameDay, parse } from "date-fns"
 
-import eventList from "data/events.json"
-import EventCard from "./eventcard"
-import TitleText from "../_components/title-text"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs"
-import { isAfter, isBefore, isSameDay, parse } from "date-fns"
-
+import eventList from "data/events.json"
+import EventCard from "./card"
 import { type Event } from "~/lib/types"
 
+const date = new Date()
+
 const parseEventDate = (date: string, time: string) => {
-  return parse(`${date} ${time.toUpperCase()}`, "dd/MM/yy h:mma", new Date())
+  return parse(`${date} ${time.toUpperCase()}`, "dd/MM/yy h:mma", date)
 }
 
-const sorted = (array: Event[]) =>
+const sort = (array: Event[], isReversed = false) =>
   array.sort((event1, event2) => {
     const date1 = parseEventDate(event1.date, event1.time.start)
     const date2 = parseEventDate(event2.date, event2.time.start)
 
+    if (isReversed) return isAfter(date1, date2) ? -1 : 1
     return isAfter(date1, date2) ? 1 : -1
   })
 
+const filterYears = (array: Array<Event>) => {
+  const data: Record<string, Array<Event>> = {}
+
+  array.forEach((event) => {
+    const year = parse(event.date, "dd/MM/yy", date).getFullYear().toString()
+
+    if (data.hasOwnProperty(year)) {
+      data[year]!.push(event)
+    } else {
+      data[year] = [event]
+    }
+  })
+
+  return data
+}
+
 const events = {
-  past: sorted(
-    eventList.filter((event) =>
-      isBefore(parse(`${event.date} ${event.time.start}`, "dd/MM/yy h:mma", new Date()), new Date()),
+  past: filterYears(
+    sort(
+      eventList.filter((event) => isBefore(parse(`${event.date} ${event.time.start}`, "dd/MM/yy h:mma", date), date)),
+      true,
     ),
   ),
-  upcoming: sorted(
-    eventList.filter((event) => {
-      const date = parse(`${event.date} ${event.time.start}`, "dd/MM/yy h:mma", new Date())
-      return isAfter(date, new Date()) || isSameDay(date, new Date())
-    }),
+  upcoming: filterYears(
+    sort(
+      eventList.filter((event) => {
+        const _date = parse(`${event.date} ${event.time.start}`, "dd/MM/yy h:mma", date)
+        return isAfter(_date, date) || isSameDay(_date, date)
+      }),
+    ),
   ),
 }
 
-const EventsPage = () => {
-  return (
-    <main className="main">
-      <TitleText typed>./events</TitleText>
-      <div className="bg-secondary text-primary dark:bg-alt-dark dark:text-secondary">
-        <div className="container mx-auto space-y-12 px-3 py-12 md:py-24">
-          <div className="mb-8 text-left">
-            <Tabs defaultValue="upcoming">
-              <TabsList className="mb-2">
-                <TabsTrigger value="past">Past</TabsTrigger>
-                <TabsTrigger value="upcoming">Upcoming</TabsTrigger>
-              </TabsList>
-              <TabsContent value="past">
-                <div className="space-y-6">
-                  {events.past.length === 0 ? (
-                    <h2 className="font-mono text-4xl text-primary">No past events</h2>
-                  ) : (
-                    events.past.map((event) => (
-                      <div
-                        key={event.date + event.time.start}
-                        className="relative border-l border-primary border-opacity-40 dark:border-secondary dark:border-opacity-40"
-                      >
-                        <span className="absolute left-0 -translate-x-1/2 translate-y-full rotate-90 transform bg-secondary px-4 font-mono text-lg text-primary dark:bg-alt-dark lg:translate-y-0 lg:rotate-0">
-                          {event.date}
-                        </span>
-                        <EventCard key={event.slug} {...event} />
-                      </div>
-                    ))
-                  )}
-                </div>
-              </TabsContent>
-              <TabsContent value="upcoming">
-                <div className="space-y-6">
-                  {events.upcoming.length === 0 ? (
-                    <h2 className="font-mono text-4xl text-black dark:text-white">No upcoming events</h2>
-                  ) : (
-                    events.upcoming.map((event) => (
-                      <div
-                        key={event.date + event.time.start}
-                        className="relative border-l border-primary border-opacity-40 dark:border-secondary dark:border-opacity-40"
-                      >
-                        <span className="absolute left-0 -translate-x-1/2 translate-y-full rotate-90 transform bg-secondary px-4 font-mono text-lg text-primary dark:bg-alt-dark lg:translate-y-0 lg:rotate-0">
-                          {event.date}
-                        </span>
-                        <EventCard key={event.slug} {...event} />
-                      </div>
-                    ))
-                  )}
-                </div>
-              </TabsContent>
-            </Tabs>
-          </div>
-        </div>
-      </div>
-    </main>
-  )
+const smallDate = (date: string) => {
+  return format(parse(date, "dd/MM/yy", new Date()), "MMM dd")
 }
 
-export default EventsPage
+export default function EventsPage() {
+  return (
+    <Tabs defaultValue="upcoming" className="container py-12">
+      <TabsList className="mb-2 w-full max-w-xs">
+        <TabsTrigger value="past" className="w-full">
+          Past
+        </TabsTrigger>
+        <TabsTrigger value="upcoming" className="w-full">
+          Upcoming
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value="past">
+        <div className="space-y-6">
+          {Object.keys(events.past).length === 0 ? (
+            <h2 className="font-mono text-3xl text-primary">No past events</h2>
+          ) : (
+            Object.entries(events.past).map(([year, events]) => (
+              <div key={year}>
+                <h3 className="mb-2 ml-4 font-mono text-lg lg:ml-16">{year}</h3>
+                <div className="space-y-4">
+                  {events.map((event) => (
+                    <div
+                      key={event.date + event.time.start}
+                      className="relative border-l border-black/25 dark:border-white/25"
+                    >
+                      <span className="absolute left-0 -translate-x-1/2 translate-y-2/3 rotate-90 bg-background pb-1 pr-2 font-mono lg:-translate-x-1 lg:translate-y-0 lg:rotate-0 lg:px-0">
+                        {smallDate(event.date)}
+                      </span>
+                      <EventCard key={event.slug} {...event} />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))
+          )}
+        </div>
+      </TabsContent>
+      <TabsContent value="upcoming">
+        <div className="space-y-6">
+          {Object.keys(events.upcoming).length === 0 ? (
+            <h2 className="font-mono text-3xl text-black dark:text-white">No upcoming events</h2>
+          ) : (
+            Object.entries(events.upcoming).map(([year, events]) => (
+              <div key={year}>
+                <h3 className="mb-2 ml-4 font-mono text-lg lg:ml-16">{year}</h3>
+                {events.map((event) => (
+                  <div
+                    key={event.date + event.time.start}
+                    className="relative border-l border-black/25 dark:border-white/25"
+                  >
+                    <span className="absolute left-0 -translate-x-1/2 translate-y-2/3 rotate-90 bg-background pb-1 pr-2 font-mono lg:-translate-x-1 lg:translate-y-0 lg:rotate-0 lg:px-0">
+                      {smallDate(event.date)}
+                    </span>
+                    <EventCard key={event.slug} {...event} />
+                  </div>
+                ))}
+              </div>
+            ))
+          )}
+        </div>
+      </TabsContent>
+    </Tabs>
+  )
+}
