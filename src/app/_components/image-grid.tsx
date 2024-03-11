f17172eb9e1fa6c7623e9d4b0f82a109a5ae92c3
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2024-03-12 03:39:59 +0800

    update logo layout in about page

--- src/app/_components/image-grid.tsx
+++ src/app/_components/image-grid.tsx
@@ -1,9 +1,8 @@
-"use client"
-
 import { useTheme } from "next-themes"
 import Image from "next/image"
-import { type ImageProps } from "~/lib/types"
 import Link from "next/link"
+
+import type { ImageProps } from "~/lib/types"
 
 interface ImageGridProps {
   images: ImageProps[]
@@ -13,15 +12,14 @@ const ImageGrid = (props: ImageGridProps) => {
   const { theme } = useTheme()
 
   return (
-    <div className="flex h-24 gap-3 md:gap-12">
+    <div className="grid grid-cols-[repeat(auto-fit,minmax(12rem,1fr))] gap-8">
       {props.images.map((image) => (
-        <div key={image.alt} className="relative w-full basis-1/2">
+        <div key={image.alt} className="relative aspect-video h-24 w-full">
           {image.link ? (
             <Link href={image.link} passHref>
               <Image
                 src={theme === "dark" && image.srcDark ? image.srcDark : image.src}
                 alt={image.alt}
-                priority
                 fill
                 className="object-contain object-center brightness-110 contrast-[0.2] grayscale transition duration-300 hover:filter-none"
               />
@@ -30,7 +28,6 @@ const ImageGrid = (props: ImageGridProps) => {
             <Image
               src={theme === "dark" && image.srcDark ? image.srcDark : image.src}
               alt={image.alt}
-              priority
               fill
               className="object-contain object-center brightness-110 contrast-[0.2] grayscale transition duration-300 hover:filter-none"
             />
