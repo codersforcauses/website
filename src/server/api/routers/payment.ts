86fd69032e320ffba0000e60f3f8d5cac10fe11b
Dylan To <dylan.to@hotmail.com>
2024-03-21 16:00:13 +0800

    Fix get cards reponse when no cards

--- src/server/api/routers/payment.ts
+++ src/server/api/routers/payment.ts
@@ -1,12 +1,11 @@
 import { TRPCError } from "@trpc/server"
 import { Ratelimit } from "@upstash/ratelimit"
-import { eq } from "drizzle-orm"
 import { z } from "zod"
 import { Client, Environment } from "square"
 import { randomUUID } from "crypto"
 
 import { createTRPCRouter, protectedRatedProcedure } from "~/server/api/trpc"
-import { payments, users } from "~/server/db/schema"
+import { payments } from "~/server/db/schema"
 import { env } from "~/env"
 
 const { cardsApi, paymentsApi } = new Client({
@@ -54,33 +53,26 @@ export const paymentRouter = createTRPCRouter({
       }),
     )
     .mutation(async ({ ctx, input }) => {
-      try {
-        const [user] = await ctx.db.select().from(users).where(eq(users.id, ctx.user.id))
-
-        const { result } = await paymentsApi.createPayment({
-          idempotencyKey: randomUUID(),
-          locationId: env.NEXT_PUBLIC_SQUARE_LOCATION_ID,
-          sourceId: input.sourceID,
-          customerId: user?.square_customer_id,
-          referenceId: ctx.user.id,
-          statementDescriptionIdentifier: input.label,
-          note: `Payment for ${input.label}`,
-          amountMoney: {
-            currency: input.currency,
-            amount: BigInt(parseFloat(input.amount) * 100),
-          },
-        })
-        await ctx.db.insert(payments).values({
-          user_id: ctx.user.id,
-          label: input.label,
-          amount: input.amount,
+      const { result } = await paymentsApi.createPayment({
+        idempotencyKey: randomUUID(),
+        locationId: env.NEXT_PUBLIC_SQUARE_LOCATION_ID,
+        sourceId: input.sourceID,
+        customerId: ctx.user.square_customer_id,
+        referenceId: ctx.user.id,
+        statementDescriptionIdentifier: input.label,
+        note: `Payment for ${input.label}`,
+        amountMoney: {
           currency: input.currency,
-        })
-        return result.payment?.id
-      } catch (error) {
-        // add error handling
-        console.log(error)
-      }
+          amount: BigInt(parseFloat(input.amount) * 100),
+        },
+      })
+      await ctx.db.insert(payments).values({
+        user_id: ctx.user.id,
+        label: input.label,
+        amount: input.amount,
+        currency: input.currency,
+      })
+      return result.payment?.id
     }),
 
   storeCard: protectedRatedProcedure(Ratelimit.fixedWindow(2, "30s"))
@@ -95,37 +87,35 @@ export const paymentRouter = createTRPCRouter({
       }),
     )
     .mutation(async ({ ctx, input }) => {
-      try {
-        const [user] = await ctx.db.select().from(users).where(eq(users.id, ctx.user.id))
-        const { result } = await cardsApi.createCard({
-          idempotencyKey: randomUUID(),
-          sourceId: input.sourceID,
-          // verificationToken: input.verificationToken,
-          card: {
-            customerId: user?.square_customer_id,
-            referenceId: ctx.user.id,
-          },
-        })
-        return result.card?.id
-      } catch (error) {
-        // add error handling
-        console.log(error)
-      }
+      const { result } = await cardsApi.createCard({
+        idempotencyKey: randomUUID(),
+        sourceId: input.sourceID,
+        // verificationToken: input.verificationToken,
+        card: {
+          customerId: ctx.user.square_customer_id,
+          referenceId: ctx.user.id,
+        },
+      })
+      return result.card?.id
     }),
 
   getCards: protectedRatedProcedure(Ratelimit.fixedWindow(2, "30s")).query(async ({ ctx }) => {
-    try {
-      const [user] = await ctx.db.select().from(users).where(eq(users.id, ctx.user.id))
-      const { result } = await cardsApi.listCards(undefined, user?.square_customer_id)
-      if (result.cards?.length === 0) return []
-      return result.cards?.map((card) => ({
-        id: card.id ?? "",
-        number: `**** **** **** ${card.last4}`,
-        brand: card.cardBrand,
-        expiry: `${Number(card.expMonth)}/${Number(card.expYear)}`,
-      }))
-    } catch (error) {
-      // add error handling
+    const { result } = await cardsApi.listCards(undefined, ctx.user.square_customer_id)
+
+    if (result.errors) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Error fetching cards from Square",
+        cause: result.errors,
+      })
     }
+
+    if (!result.cards?.length) return []
+    return result.cards.map((card) => ({
+      id: card.id ?? "",
+      number: `**** **** **** ${card.last4}`,
+      brand: card.cardBrand,
+      expiry: `${Number(card.expMonth)}/${Number(card.expYear)}`,
+    }))
   }),
 })
