5c75409a1724edd9a5c7a782d4225554117cf21b
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2024-03-22 20:53:37 +0800

    upgrade deps and add drizzle eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,16 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
+  root: true,
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "drizzle"],
   extends: [
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:drizzle/recommended",
   ],
   rules: {
     // These opinionated rules are enabled in stylistic-type-checked above.
