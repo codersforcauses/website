430070776207674bb04d8c93eec0b63a90e7bfad
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2024-04-04 04:01:08 +0800

    remove comment

--- src/app/dashboard/admin/@analytics/page.tsx
+++ src/app/dashboard/admin/@analytics/page.tsx
@@ -8,7 +8,6 @@ export default async function AdminAnalyticsTable() {
       <div className="flex h-[50px] items-center p-1">
         <h2 className="text-2xl font-semibold">Analytics</h2>
       </div>
-      Need to update the SQL for this on BE
       <div className="grid grid-cols-5 gap-4">
         <Count />
         <UsersGraph />
