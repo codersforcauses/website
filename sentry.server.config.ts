e03037de40319cadc06bc25fb66a1e261d79ea70
Dylan To <dylan.to@hotmail.com>
2024-07-26 21:09:19 +0800

    It better work now

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,12 +5,14 @@
 import * as Sentry from "@sentry/nextjs"
 
 Sentry.init({
-  enabled: process.env.VERCEL_ENV === "production",
-  dsn: "https://237a767d0f7c4a79b052b6df86adb720@o1131895.ingest.sentry.io/6176944",
+  dsn: "https://237a767d0f7c4a79b052b6df86adb720@o1131895.ingest.us.sentry.io/6176944",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
 })
