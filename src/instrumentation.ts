a723f95ef0183664bea6dc39b35de61f0d07d455
Dylan To <dylan.to@hotmail.com>
2024-07-26 21:29:12 +0800

    Use instrumentation hook

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -3,8 +3,16 @@ import * as Sentry from "@sentry/nextjs"
 export function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     Sentry.init({
-      dsn: "__DSN__",
-      // Your Node.js Sentry configuration...
+      dsn: "https://237a767d0f7c4a79b052b6df86adb720@o1131895.ingest.us.sentry.io/6176944",
+
+      // Adjust this value in production, or use tracesSampler for greater control
+      tracesSampleRate: 1,
+
+      // Setting this option to true will print useful information to the console while you're setting up Sentry.
+      debug: false,
+
+      // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+      // spotlight: process.env.NODE_ENV === 'development',
     })
   }
 
