834a3fda6228d4f01ac838ffe00bd760cb499734
K-Straiton <straiton98@gmail.com>
2024-08-27 23:48:46 +0800

    Add sponsor us page and first section

---
+++ src/app/sponsor-us/layout.tsx
@@ -0,0 +1,31 @@
+import type { Viewport, Metadata } from "next"
+import TitleText from "../_components/title-text"
+import { type PropsWithChildren } from "~/lib/types"
+import { customMetadata } from "~/lib/metadata"
+
+export const viewport: Viewport = {
+  themeColor: "black",
+}
+
+export const metadata: Metadata = {
+  title: "Sponsor Us",
+  ...customMetadata({
+    name: "Sponsor Us",
+    page: "sponsor",
+    description:
+      "Coders for Causes is a software engineering club at UWA specializing in web development. We build custom software for charities and not for profits and give students the skills they need to succeed in the industry.",
+    image:
+      "https://og-social-cards.vercel.app/**.%2Fabout**.png?theme=dark&md=1&fontSize=125px&images=https%3A%2Fcodersforcauses.org%2Flogo%2Fcfc_logo_white_full.svg",
+  }),
+}
+
+const Layout = ({ children }: PropsWithChildren) => {
+  return (
+    <main className="main">
+      <TitleText typed>./sponsor-us</TitleText>
+      {children}
+    </main>
+  )
+}
+
+export default Layout
