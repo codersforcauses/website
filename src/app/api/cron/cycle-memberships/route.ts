33bdb2641eedd9f7a1d64d330b7a9baa6ebbadf9
Dylan To <dylan.to@hotmail.com>
2024-12-31 15:01:14 +0800

    Add sentry monitoring

--- src/app/api/cron/cycle-memberships/route.ts
+++ src/app/api/cron/cycle-memberships/route.ts
@@ -3,6 +3,7 @@ import { env } from "~/env"
 import { db } from "~/server/db"
 import { users } from "~/server/db/schema"
 import { eq } from "drizzle-orm"
+import * as Sentry from "@sentry/nextjs"
 
 export const dynamic = "force-dynamic"
 
@@ -14,14 +15,37 @@ export async function GET(request: NextRequest) {
     })
   }
 
+  const checkInId = Sentry.captureCheckIn({
+    monitorSlug: "cycle-memberships",
+    status: "in_progress",
+  })
+
   // TODO backup with xata cli and put into aws bucket
 
-  const dbRes = await db.select().from(users).where(eq(users.role, "member"))
-  // const dbRes = await db.update(users).set({ role: null }).where(eq(users.role, "member")).returning()
+  try {
+    const dbRes = await db.select().from(users).where(eq(users.role, "member"))
+    // const dbRes = await db.update(users).set({ role: null }).where(eq(users.role, "member")).returning()
 
-  return Response.json({
-    success: true,
-    message: `Memberships for ${new Date().getFullYear()} have been cycled.`,
-    count: dbRes.length,
-  })
+    Sentry.captureCheckIn({
+      checkInId,
+      monitorSlug: "cycle-memberships",
+      status: "ok",
+    })
+
+    return Response.json({
+      success: true,
+      message: `Memberships for ${new Date().getFullYear()} have been cycled.`,
+      count: dbRes.length,
+    })
+  } catch (e) {
+    Sentry.captureCheckIn({
+      checkInId,
+      monitorSlug: "cycle-memberships",
+      status: "error",
+    })
+    console.error(e)
+    return new Response("Internal Server Error", {
+      status: 500,
+    })
+  }
 }
