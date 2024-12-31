1fdbb7dea30a0b2b0ff14e939c68db88e4d2e15e
Dylan To <dylan.to@hotmail.com>
2024-12-31 15:15:56 +0800

    Remove try catch

--- src/app/api/cron/cycle-memberships/route.ts
+++ src/app/api/cron/cycle-memberships/route.ts
@@ -21,31 +21,18 @@ export async function GET(request: NextRequest) {
   })
 
   // TODO backup with xata cli and put into aws bucket
+  const dbRes = await db.select().from(users).where(eq(users.role, "member"))
+  // const dbRes = await db.update(users).set({ role: null }).where(eq(users.role, "member")).returning()
 
-  try {
-    const dbRes = await db.select().from(users).where(eq(users.role, "member"))
-    // const dbRes = await db.update(users).set({ role: null }).where(eq(users.role, "member")).returning()
+  Sentry.captureCheckIn({
+    checkInId,
+    monitorSlug: "cycle-memberships",
+    status: "ok",
+  })
 
-    Sentry.captureCheckIn({
-      checkInId,
-      monitorSlug: "cycle-memberships",
-      status: "ok",
-    })
-
-    return Response.json({
-      success: true,
-      message: `Memberships for ${new Date().getFullYear()} have been cycled.`,
-      count: dbRes.length,
-    })
-  } catch (e) {
-    Sentry.captureCheckIn({
-      checkInId,
-      monitorSlug: "cycle-memberships",
-      status: "error",
-    })
-    console.error(e)
-    return new Response("Internal Server Error", {
-      status: 500,
-    })
-  }
+  return Response.json({
+    success: true,
+    message: `Memberships for ${new Date().getFullYear()} have been cycled.`,
+    count: dbRes.length,
+  })
 }
