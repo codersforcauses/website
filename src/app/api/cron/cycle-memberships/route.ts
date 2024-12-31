af7228c3951d68ec71fe31a62e6de3d29570f2e6
Dylan To <dylan.to@hotmail.com>
2024-12-31 15:40:30 +0800

    Another way

--- src/app/api/cron/cycle-memberships/route.ts
+++ src/app/api/cron/cycle-memberships/route.ts
@@ -4,6 +4,7 @@ import { db } from "~/server/db"
 import { users } from "~/server/db/schema"
 import { eq } from "drizzle-orm"
 import * as Sentry from "@sentry/nextjs"
+import { type User } from "~/lib/types"
 
 export const dynamic = "force-dynamic"
 
@@ -15,18 +16,25 @@ export async function GET(request: NextRequest) {
     })
   }
 
+  let dbRes: User[] = []
+
   await Sentry.withMonitor("cycle-memberships", async () => {
     // TODO backup with xata cli and put into aws bucket
-    const dbRes = await db.select().from(users).where(eq(users.role, "member"))
+    dbRes = await db.select().from(users).where(eq(users.role, "member"))
     // const dbRes = await db.update(users).set({ role: null }).where(eq(users.role, "member")).returning()
-    return Response.json({
+  })
+
+  if (!dbRes.length) {
+    return new Response("Internal Server Error", {
+      status: 500,
+    })
+  }
+
+  return new Response(
+    JSON.stringify({
       success: true,
       message: `Memberships for ${new Date().getFullYear()} have been cycled.`,
       count: dbRes.length,
-    })
-  })
-
-  return new Response("Internal Server Error", {
-    status: 500,
-  })
+    }),
+  )
 }
