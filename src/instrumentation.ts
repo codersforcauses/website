d892e24b7d351e567c284b17fb03666ec9fb4984
jsun1590 <40915904+jsun1590@users.noreply.github.com>
2025-04-18 18:33:50 +0800

    fix: sentry (#1614)

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,30 +1,13 @@
 import * as Sentry from "@sentry/nextjs"
 
-export function register() {
+export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    Sentry.init({
-      dsn: "https://237a767d0f7c4a79b052b6df86adb720@o1131895.ingest.us.sentry.io/6176944",
-
-      // Adjust this value in production, or use tracesSampler for greater control
-      tracesSampleRate: 1,
-
-      // Setting this option to true will print useful information to the console while you're setting up Sentry.
-      debug: false,
-
-      // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
-      // spotlight: process.env.NODE_ENV === 'development',
-    })
+    await import("../sentry.server.config")
   }
 
   if (process.env.NEXT_RUNTIME === "edge") {
-    Sentry.init({
-      dsn: "https://237a767d0f7c4a79b052b6df86adb720@o1131895.ingest.sentry.io/6176944",
-
-      // Adjust this value in production, or use tracesSampler for greater control
-      tracesSampleRate: 1,
-
-      // Setting this option to true will print useful information to the console while you're setting up Sentry.
-      debug: false,
-    })
+    await import("../sentry.edge.config")
   }
 }
+
+export const onRequestError = Sentry.captureRequestError
