a1ce9abf091ac04f82970f43f0862c3086b440ae
Jack Sun <jsun1590@gmail.com>
2025-04-18 18:22:41 +0800

    fix: sentry

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
+import { withSentryConfig } from "@sentry/nextjs"
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
 await import("./src/env.js")
-import { withSentryConfig } from "@sentry/nextjs"
 
 /** @type {import("next").NextConfig} */
-const config = {}
+const config = {
+  experimental: {
+    instrumentationHook: true,
+  },
+}
 
 export default withSentryConfig(config, {
   telemetry: false,
