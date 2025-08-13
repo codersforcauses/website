721bd74f8f6df0c1bcc32e13e68d26c41a09a8c2
ErikaKK <491649804@qq.com>
2025-08-13 18:35:45 +0800

    change back to the admin tab

--- src/app/dashboard/admin/layout.tsx
+++ src/app/dashboard/admin/layout.tsx
@@ -1,11 +1,8 @@
 "use client"
 
-import Link from "next/link"
 import * as React from "react"
-import { useState } from "react"
 import type { ImperativePanelHandle } from "react-resizable-panels"
 
-import { Button } from "~/components/ui/button"
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "~/components/ui/resizable"
 import { Separator } from "~/components/ui/separator"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs"
@@ -13,101 +10,53 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs"
 import type { PropsWithChildren } from "~/lib/types"
 import { cn } from "~/lib/utils"
 
-const Layout = ({ children }: PropsWithChildren) => {
+interface AdminDashLayoutProps extends PropsWithChildren {
+  users: React.ReactNode
+  projects: React.ReactNode
+  // events: React.ReactNode
+  analytics: React.ReactNode
+  tools: React.ReactNode
+}
+
+const Layout = ({ children, ...props }: AdminDashLayoutProps) => {
+  const sidebarRef = React.useRef<ImperativePanelHandle>(null)
+  const [collapsed, setCollapsed] = React.useState(false)
+
+  const sidebarItems = [
+    { text: "Users", icon: "group", component: props.users },
+    { text: "Projects", icon: "devices", component: props.projects },
+    // { text: "Events", icon: "event", component: <Placeholder /> },
+    { text: "Analytics", icon: "analytics", component: props.analytics },
+    { text: "Tools", icon: "service_toolbox", component: props.tools },
+  ]
+
   return (
-    <main className="main container py-4 flex flex-col md:flex-row">
-      <AdminHeader />
-      <div className="w-full md:ml-3">{children}</div>
+    <main className="main container py-4 ">
+      <Tabs defaultValue={sidebarItems[0]?.text} className="flex flex-col md:flex-row">
+        <TabsList className="h-full flex-grow p-1 md:h-auto md:flex-grow-0">
+          <div className="flex md:grid md:h-min md:w-52 md:max-w-xs md:grid-cols-1 md:gap-1 md:self-start">
+            <div className="flex items-center p-1">
+              <div className="grid size-[42px] min-w-[42px] place-items-center bg-black font-mono font-semibold text-white dark:bg-white dark:text-black">
+                cfc
+              </div>
+              <h1 className="ml-2 font-mono font-bold leading-tight text-black">Admin Dashboard</h1>
+            </div>
+            {sidebarItems.map(({ text, icon }) => (
+              <TabsTrigger
+                key={text}
+                value={text}
+                className="group flex justify-start focus:ring-1 focus:ring-muted-foreground data-[state=active]:flex-grow md:h-[42px]"
+              >
+                <span className="material-symbols-sharp size-6">{icon}</span>
+                <span className="ml-2 hidden group-data-[state=active]:block sm:block">{text}</span>
+              </TabsTrigger>
+            ))}
+          </div>
+        </TabsList>
+      </Tabs>
+      {children}
     </main>
   )
 }
 
 export default Layout
-
-interface AdminHeaderItem {
-  href: string
-  text: string
-  icon: string
-}
-
-const links: Array<AdminHeaderItem> = [
-  { href: "/dashboard/admin/users", text: "Users", icon: "group" },
-  { href: "/dashboard/admin/projects", text: "Projects", icon: "work" },
-  { href: "/dashboard/admin/analytics", text: "Analytics", icon: "analytics" },
-  { href: "/dashboard/admin/tools", text: "Tools", icon: "service_toolbox" },
-  // { href: "events", text: "Events", icon: "event" },
-]
-
-const AdminHeader = () => {
-  const [open, setOpen] = useState(false)
-  return (
-    <>
-      <div className=" flex md:flex-col items-center md:items-start bg-muted max-w-52">
-        <Link href={"/dashboard/admin"} className="flex items-center p-1  md:border-b-2 border-b-background w-full">
-          <div
-            className={cn(
-              "grid size-[42px] min-w-[42px] place-items-center bg-black font-mono font-semibold text-white dark:bg-white dark:text-black aspect-square ",
-            )}
-          >
-            cfc
-          </div>
-          <h1 className="ml-2 font-mono font-bold leading-tight">Admin Dashboard</h1>
-        </Link>
-        <span className="material-symbols-sharp  md:hidden cursor-pointer" onClick={() => setOpen(!open)}>
-          arrow_forward_ios
-        </span>
-        <div className=" bg-muted w-full p-1.5 gap-1 hidden md:flex flex-col text-sm">
-          {links.map(({ text, href, icon }) => (
-            <Link
-              key={text}
-              href={href}
-              className="flex gap-2 p-2 w-full items-center text-muted-foreground cursor-pointer hover:bg-background"
-            >
-              <span className="material-symbols-sharp size-6">{icon}</span>
-              <div>{text}</div>
-            </Link>
-          ))}
-        </div>
-      </div>
-      {open && (
-        <div
-          className={` top-1/2 left-0 transform 
-            -translate-y-1/2 
-            ${open ? "translate-x-0" : "translate-x-full"} 
-            transition-transform duration-1000 ease-out bg-muted p-1.5 fixed w-full h-full z-50  `}
-        >
-          <Link
-            href={"/dashboard/admin"}
-            className="flex items-center p-1  border-b-2 border-b-background w-full"
-            onClick={() => setOpen(!open)}
-          >
-            <div
-              className={cn(
-                "grid size-[42px] min-w-[42px] place-items-center bg-black font-mono font-semibold text-white dark:bg-white dark:text-black aspect-square ",
-              )}
-            >
-              cfc
-            </div>
-            <h1 className="ml-2 font-mono font-bold leading-tight">Admin Dashboard</h1>
-          </Link>
-          <div className="grid h-auto w-full grid-cols-1 gap-1 p-2 text-sm">
-            {links.map(({ text, href, icon }) => (
-              <Link
-                key={text}
-                href={href}
-                className="flex gap-2 p-2 items-center text-muted-foreground cursor-pointer hover:bg-background"
-                onClick={() => setOpen(!open)}
-              >
-                <span className="material-symbols-sharp size-6 cursor-pointer">{icon}</span>
-                <div>{text}</div>
-              </Link>
-            ))}
-          </div>
-          <Button onClick={() => setOpen(!open)} className="mt-40 w-full">
-            Back
-          </Button>
-        </div>
-      )}
-    </>
-  )
-}
--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,23 +1,5 @@
 import * as React from "react"
 
-import { api } from "~/trpc/server"
-
-import { Card } from "./analytics/count"
-
 export default async function AdminDashboard() {
-  const count = await api.admin.analytics.getUserCount.query()
-
-  return (
-    <>
-      <div className="flex h-[50px] items-center p-1">
-        <h2 className="text-2xl font-semibold">Admin Dashboard</h2>
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 ">
-        <Card heading="Users" count={count.users} icon="group" href="/dashboard/admin/users" />
-        <Card heading="Members" count={count.members} icon="group" href="/dashboard/admin/users" />
-        <Card heading="Projects" count={"N/A"} icon="devices" href="/dashboard/admin/projects" />
-        <Card heading="Events" count={"N/A"} icon="event" />
-      </div>
-    </>
-  )
+  return <></>
 }
