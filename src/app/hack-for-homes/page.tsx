068bb2203729ea617d92d2fa65bb771d80136251
Sam Jackson <114900452+samjjacko@users.noreply.github.com>
2025-08-29 01:56:51 +0800

    1640 hackathon itinerary (#1644)
    * re-added the rubric
    
    * copied page from other repository
    
    * moved stuff around
    
    * add agenda and participant page
    
    * added hack-for-homes landing page with some drop downs for info
    
    * minor adjustments
    
    * touched up styling a bit and added sponsors
    
    * Data entered
    
    * updated nav
    
    * update styles and add sponsors
    
    * cleanup and rename files
    
    * Rename files
    
    * Remove item from header
    
    ---------
    
    Co-authored-by: ErikaKK <491649804@qq.com>

---
+++ src/app/hack-for-homes/page.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image"
+import Link from "next/link"
+
+import { Button } from "~/components/ui/button"
+
+import GuidingQuestionsAccordion from "./guiding-qs"
+import ResourcesAccordion from "./resources"
+import RubricsAccordion from "./rubrics"
+import Sponsors from "./sponsors"
+import VenueAccordion from "./venue"
+
+export default function Itinerary() {
+  return (
+    <>
+      <div className="relative text-primary dark:bg-alt-dark dark:text-primary">
+        <div className="container grid gap-4 py-12 md:py-16 lg:grid-cols-2">
+          <div id="_what_we_do" className="space-y-2">
+            <h2 className="mb-4 font-mono text-3xl font-black">Problem Statement</h2>
+            <p className="text-lg">
+              How can we improve housing accessibility and living conditions for Western Australians struggling to find
+              affordable, safe, and stable housing during the current housing crisis?
+            </p>
+            <div className="flex pl-5 space-x-5 pt-4">
+              <Button>
+                <Link href="/hack-for-homes/agenda">Agenda</Link>
+              </Button>
+              <Button>
+                <Link href="/hack-for-homes/participants">Participants</Link>
+              </Button>
+            </div>
+          </div>
+          <div className="hidden lg:block relative h-full w-full max-w-lg">
+            <Image alt="Hackathon Poster" fill className="object-contain" src="/events/img_792.png" />
+          </div>
+        </div>
+      </div>
+      <div className="container w-full flex flex-col items-start justify-start py-8 gap-4">
+        <VenueAccordion />
+        <GuidingQuestionsAccordion />
+        <ResourcesAccordion />
+        <RubricsAccordion />
+      </div>
+      <div className="bg-secondary bg-white py-6 text-primary dark:bg-alt-dark dark:text-secondary md:py-12 ">
+        <div className="container mx-auto space-y-12">
+          <h3 className="font-mono text-2xl font-black dark:text-white">Proudly Supported By</h3>
+          <Sponsors />
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ src/app/hack-for-homes/rubrics.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image"
+
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "~/components/ui/accordion"
+
+export default function RubricsAccordion() {
+  return (
+    <Accordion type="single" collapsible className="w-full">
+      <AccordionItem value="rubric">
+        <AccordionTrigger className="px-4 border-2">
+          <h2 className="text-xl font-semibold"> Rubrics</h2>
+        </AccordionTrigger>
+        <AccordionContent>
+          <Image src="/events/hack_for_homes_2025_rubric.png" alt="Rubric Image" height={500} width={800} />
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  )
+}
