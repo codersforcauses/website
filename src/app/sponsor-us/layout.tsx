6d8a64748e15eed2a0a5cdafe2ac89276f840a07
Erica <98962967+ErikaKK@users.noreply.github.com>
2025-09-02 23:03:01 +0800

    1540 create sponsor us page (#1650)
    * Add sponsor us page and first section
    
    * Add sponsor us page and first section
    
    * edit sponsor page and route
    
    * change a symbol
    
    * edit bits and pieces
    
    * again small changes
    
    * change some button redirect
    
    * change to ssr
    
    * add alt to bg image
    
    ---------
    
    Co-authored-by: K-Straiton <straiton98@gmail.com>

---
+++ src/app/sponsor-us/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata, Viewport } from "next"
+
+import { customMetadata } from "~/lib/metadata"
+import { type PropsWithChildren } from "~/lib/types"
+
+import TitleText from "../_components/title-text"
+
+export const viewport: Viewport = {
+  themeColor: "black",
+}
+
+export const metadata: Metadata = {
+  title: "Sponsor Us",
+  ...customMetadata({
+    name: "Sponsor Us",
+    page: "sponsor-us",
+    description:
+      "Coders for Causes is a software engineering club at UWA specializing in web development. We build custom software for charities and not for profits and give students the skills they need to succeed in the industry.",
+    image:
+      "https://og-social-cards.vercel.app/**.%2Fsponsor-us**.png?theme=dark&md=1&fontSize=125px&images=https%3A%2Fcodersforcauses.org%2Flogo%2Fcfc_logo_white_full.svg",
+  }),
+}
+
+const Layout = ({ children }: PropsWithChildren) => {
+  return <main className="main">{children}</main>
+}
+
+export default Layout
