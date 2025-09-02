b7b140ff5f806333b1d2faa44b9b80dd83676c38
ErikaKK <491649804@qq.com>
2025-09-02 22:11:15 +0800

    again small changes

--- src/app/sponsor-us/layout.tsx
+++ src/app/sponsor-us/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
     description:
       "Coders for Causes is a software engineering club at UWA specializing in web development. We build custom software for charities and not for profits and give students the skills they need to succeed in the industry.",
     image:
-      "https://og-social-cards.vercel.app/**.%2Fabout**.png?theme=dark&md=1&fontSize=125px&images=https%3A%2Fcodersforcauses.org%2Flogo%2Fcfc_logo_white_full.svg",
+      "https://og-social-cards.vercel.app/**.%2Fsponsor-us**.png?theme=dark&md=1&fontSize=125px&images=https%3A%2Fcodersforcauses.org%2Flogo%2Fcfc_logo_white_full.svg",
   }),
 }
 
